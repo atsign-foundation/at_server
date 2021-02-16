bf810ba2ac7b5f03fe483ae9833c47562318c3a6
Sita Ram Pavan Kumar <kalluriramkumar@gmail.com>
2021-02-16 11:19:29 +0530

    Update at_refresh_job.dart

--- at_secondary/at_secondary_server/lib/src/refresh/at_refresh_job.dart
+++ at_secondary/at_secondary_server/lib/src/refresh/at_refresh_job.dart
@@ -126,8 +126,7 @@ class AtRefreshJob {
   void scheduleRefreshJob(int runJobHour) {
     logger.finest('scheduleKeyRefreshTask starting cron job.');
     var cron = Cron();
-    cron.schedule(Schedule.parse('*/1 * * * *'), () async {
-      //cron.schedule(Schedule.parse('0 ${runJobHour} * * *'), () async {
+      cron.schedule(Schedule.parse('0 ${runJobHour} * * *'), () async {
       logger.finest('Scheduled Refresh Job started');
       await _refreshJob(runJobHour);
       logger.finest('scheduled Refresh Job completed');
