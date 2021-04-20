166e1b6a7ee49843ba6456ebe80cf5d37bea6b05
sita ram kalluri <kalluriramkumar@gmail.com>
2021-04-20 18:13:17 +0530

    Add comments

--- at_secondary/at_secondary_server/lib/src/verb/handler/sync_verb_handler.dart
+++ at_secondary/at_secondary_server/lib/src/verb/handler/sync_verb_handler.dart
@@ -57,12 +57,11 @@ class SyncVerbHandler extends AbstractVerbHandler {
     //sort the result by commitId ascending
     syncResultList.sort(
         (entry1, entry2) => entry1['commitId'].compareTo(entry2['commitId']));
-    var result;
 
     if (syncResultList.isNotEmpty) {
       syncResultList.forEach((element) {
-        result = jsonEncode(element);
-        atConnection.write('#$result\$');
+        atConnection
+            .write('${jsonEncode(element).length}#${jsonEncode(element)}\$');
       });
     }
     return;
