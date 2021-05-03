a5745efdafa5d0b55e5832c92385642eefc75c85
nareshg <naresh0689@gmail.com>
2021-05-03 15:50:15 +0530

    restart after certs expiry issue fix

--- at_secondary/at_secondary_server/lib/src/verb/manager/verb_handler_manager.dart
+++ at_secondary/at_secondary_server/lib/src/verb/manager/verb_handler_manager.dart
@@ -25,7 +25,7 @@ import 'package:at_server_spec/at_verb_spec.dart';
 
 /// The default implementation of [VerbHandlerManager].
 class DefaultVerbHandlerManager implements VerbHandlerManager {
-  static List<VerbHandler> _verbHandlers = _loadVerbHandlers();
+  List<VerbHandler> _verbHandlers;
 
   static final DefaultVerbHandlerManager _singleton =
       DefaultVerbHandlerManager._internal();
@@ -34,6 +34,11 @@ class DefaultVerbHandlerManager implements VerbHandlerManager {
 
   factory DefaultVerbHandlerManager() {
     return _singleton;
+  }
+
+  /// Initializing verb handlers
+  void init() {
+    _verbHandlers = _loadVerbHandlers();
   }
 
   ///Accepts the command in UTF-8 format and returns the appropriate verbHandler.
@@ -52,7 +57,7 @@ class DefaultVerbHandlerManager implements VerbHandlerManager {
     return null;
   }
 
-  static List<VerbHandler> _loadVerbHandlers() {
+  List<VerbHandler> _loadVerbHandlers() {
     var secondaryPersistenceStore =
         SecondaryPersistenceStoreFactory.getInstance()
             .getSecondaryPersistenceStore(
