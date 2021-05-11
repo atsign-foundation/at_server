cb2b846660f438133ed8f53bc93dd2122a4c71c7
murali-shris <murali.25@gmail.com>
2021-05-11 18:20:20 +0530

    persistence null safety changes

--- at_secondary/at_persistence_secondary_server/lib/src/notification/at_notification_manager.dart
+++ at_secondary/at_persistence_secondary_server/lib/src/notification/at_notification_manager.dart
@@ -4,7 +4,7 @@ import 'package:at_persistence_secondary_server/src/notification/at_notification
 /// Class to represent to the NotificationManager Spec.
 abstract class NotificationManagerSpec {
   /// Notifies the key to another atsign
-  Future<String> notify(AtNotification atNotification);
+  Future<String?> notify(AtNotification atNotification);
 
   /// Returns the status of the notificationId.
   Future<NotificationStatus?> getStatus(String notificationId);
