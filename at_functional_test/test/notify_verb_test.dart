da36fdce2af6e84b30da0e2ab8c8f2d56678287e
Administrator <svpurnima94@gmail.com>
2021-07-06 17:45:07 +0530

    added notify status

--- at_functional_test/test/notify_verb_test.dart
+++ at_functional_test/test/notify_verb_test.dart
@@ -5,18 +5,22 @@ import 'package:test/test.dart';
 import 'commons.dart';
 import 'package:at_functional_test/conf/config_util.dart';
 
+var response;
+var retryCount = 1;
+var maxRetryCount = 5;
+var first_atsign = '@alice🛠';
+var first_atsign_port = 25000;
+
+var second_atsign = '@bob🛠';
+var second_atsign_port = 25003;
+
+var third_atsign = '@emoji🦄🛠';
+
+Socket _socket_first_atsign;
+Socket _socket_second_atsign;
+
+var id;
 void main() {
-  var first_atsign = '@alice🛠';
-  var first_atsign_port = 25003;
-
-  var second_atsign = '@bob🛠';
-  var second_atsign_port = 25000;
-
-  var third_atsign = '@emoji🦄🛠';
-
-  Socket _socket_first_atsign;
-  Socket _socket_second_atsign;
-
   setUp(() async {
     var root_server = ConfigUtil.getYaml()['root_server']['url'];
     _socket_first_atsign =
@@ -34,20 +38,17 @@ void main() {
     /// NOTIFY VERB
     await socket_writer(_socket_second_atsign,
         'notify:update:messageType:key:notifier:system:ttr:-1:$first_atsign:email$second_atsign:alice@yahoo.com');
-    var response = await read();
+    response = await read();
     print('notify verb response : $response');
     assert(
         (!response.contains('Invalid syntax')) && (!response.contains('null')));
-    var id = response.replaceAll('data:', '');
-    print(id);
+    id = response.replaceAll('data:', '');
 
     // notify status
-    await Future.delayed(Duration(seconds: 15));
-    await socket_writer(_socket_second_atsign, 'notify:status:$id');
-    response = await read();
+    response = await getNotifyStatus(_socket_second_atsign);
     print('notify status response : $response');
     assert(response.contains('data:delivered'));
-    
+
     ///notify:list verb
     await socket_writer(_socket_first_atsign, 'notify:list');
     response = await read();
@@ -62,16 +63,14 @@ void main() {
     //   /// NOTIFY VERB
     await socket_writer(_socket_second_atsign,
         'notify:update:messageType:text:notifier:chat:ttr:-1:$first_atsign:Hello!!');
-    var response = await read();
+    response = await read();
     print('notify verb response : $response');
-    var id = response.replaceAll('data:', '');
+    id = response.replaceAll('data:', '');
     assert(
         (!response.contains('Invalid syntax')) && (!response.contains('null')));
 
-    //   // notify status
-    await Future.delayed(Duration(seconds: 15));
-    await socket_writer(_socket_second_atsign, 'notify:status:$id');
-    response = await read();
+    // notify status
+    await getNotifyStatus(_socket_second_atsign);
     print('notify status response : $response');
     expect(response, contains('data:delivered'));
 
@@ -79,28 +78,28 @@ void main() {
     await socket_writer(_socket_first_atsign, 'notify:list');
     response = await read();
     print('notify list verb response : $response');
-    expect(response,
-        contains('"key":"$first_atsign:Hello!!","value":null,"operation":"update"'));
+    expect(
+        response,
+        contains(
+            '"key":"$first_atsign:Hello!!","value":null,"operation":"update"'));
   }, timeout: Timeout(Duration(seconds: 120)));
 
   test('notify verb for deleting a key for other atsign', () async {
     //   /// NOTIFY VERB
     await socket_writer(_socket_first_atsign,
         'notify:delete:messageType:key:notifier:system:ttr:-1:$second_atsign:email$first_atsign');
-    var response = await read();
+    response = await read();
     print('notify verb response : $response');
-    var id = response.replaceAll('data:', '');
+    id = response.replaceAll('data:', '');
     assert(
         (!response.contains('Invalid syntax')) && (!response.contains('null')));
 
-    //   // notify status
-    await Future.delayed(Duration(seconds: 15));
-    await socket_writer(_socket_first_atsign, 'notify:status:$id');
-    response = await read();
+    //  notify status
+    await getNotifyStatus(_socket_first_atsign);
     print('notify status response : $response');
     expect(response, contains('data:delivered'));
 
-    //   ///notify:list verb with regex
+    //notify:list verb with regex
     await socket_writer(_socket_second_atsign, 'notify:list:email');
     response = await read();
     print('notify list verb response : $response');
@@ -110,7 +109,6 @@ void main() {
             '"key":"$second_atsign:email$first_atsign","value":null,"operation":"delete"'));
   }, timeout: Timeout(Duration(seconds: 120)));
 
-  // // notify verb- Negative scenario
   test('notify verb without giving message type value', () async {
     /// NOTIFY VERB
     await socket_writer(_socket_first_atsign,
@@ -149,6 +147,7 @@ void main() {
         (!response.contains('Invalid syntax')) && (!response.contains('null')));
 
     ///notify:list verb with regex
+    await Future.delayed(Duration(seconds: 10));
     await socket_writer(_socket_second_atsign, 'notify:list:twitter');
     response = await read();
     print('notify list verb response : $response');
@@ -170,6 +169,7 @@ void main() {
         (!response.contains('Invalid syntax')) && (!response.contains('null')));
 
     ///notify:list verb with regex
+    await Future.delayed(Duration(seconds: 10));
     await socket_writer(_socket_second_atsign, 'notify:list:twitter');
     response = await read();
     print('notify list verb response : $response');
@@ -185,4 +185,27 @@ void main() {
     _socket_first_atsign.destroy();
     _socket_second_atsign.destroy();
   });
+}
+
+// get notify status
+Future<String> getNotifyStatus(Socket socket) async {
+  while (true) {
+    try {
+      await socket_writer(socket, 'notify:status:$id');
+      response = await read();
+      if (response.contains('data:delivered') || retryCount > maxRetryCount) {
+        break;
+      }
+      if (response.contains('data:queued') || response.contains('data:null')) {
+        print('Waiting for notification to be delivered.. $retryCount');
+        await Future.delayed(Duration(seconds: 5));
+        retryCount++;
+      }
+    } on Exception {
+      print('Waiting for result in Exception $retryCount');
+      await Future.delayed(Duration(seconds: 5));
+      retryCount++;
+    }
+  }
+  return response;
 }
