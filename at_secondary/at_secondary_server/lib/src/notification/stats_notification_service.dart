fd58555db1ed82c28d6f68db69bfd9dd836540d9
Sitaram Pavan Kumar Kalluri <kalluriramkumar@gmail.com>
2021-09-14 19:11:13 +0530

    Revert "Reduce time to 5 seconds"

--- at_secondary/at_secondary_server/lib/src/notification/stats_notification_service.dart
+++ at_secondary/at_secondary_server/lib/src/notification/stats_notification_service.dart
@@ -11,7 +11,7 @@ import 'package:at_utils/at_logger.dart';
 /// [StatsNotificationService] is a singleton class that notifies the latest commitID
 /// to the active monitor connections.
 /// The schedule job runs at a time interval specified in [notification][statsNotificationJobTimeInterval]
-/// in [config.yaml]. Defaults to 5 seconds.
+/// in [config.yaml]. Defaults to 2 Minutes.
 /// To disable the service, set [notification][statsNotificationJobTimeInterval] in [config.yaml] to -1.
 /// The [schedule] method is invoked during the server start-up and should be called only
 /// once.
@@ -58,7 +58,7 @@ class StatsNotificationService {
     // Runs the _schedule method as long as server is up and running.
     while (true) {
       await Future.delayed(
-          Duration(seconds: AtSecondaryConfig.statsNotificationJobTimeInterval),
+          Duration(minutes: AtSecondaryConfig.statsNotificationJobTimeInterval),
           _schedule);
     }
   }
@@ -91,6 +91,8 @@ class StatsNotificationService {
           ..opType = OperationType.update
           ..atValue = _atCommitLog!.lastCommittedSequenceNumber().toString();
         var notification = Notification(atNotificationBuilder.build());
+        _logger.info(
+            'writing stats notification to connection - SessionID: ${connection.getMetaData().sessionID}');
         connection
             .write('notification: ' + jsonEncode(notification.toJson()) + '\n');
       }
