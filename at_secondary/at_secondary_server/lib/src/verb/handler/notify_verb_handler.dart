c96f9d5105c6118d1c10b0771089677fa3df5793
yahu1031 <hi2minnu@gmail.com>
2021-11-22 12:37:13 +0530

    fixed notification msg type on receiver side

--- at_secondary/at_secondary_server/lib/src/verb/handler/notify_verb_handler.dart
+++ at_secondary/at_secondary_server/lib/src/verb/handler/notify_verb_handler.dart
@@ -105,7 +105,7 @@ class NotifyVerbHandler extends AbstractVerbHandler {
           'currentAtSign : $currentAtSign, forAtSign : $forAtSign, atSign : $atSign');
       if (currentAtSign == forAtSign) {
         var notificationId = await NotificationUtil.storeNotification(
-            forAtSign, atSign, key, NotificationType.received, opType,
+            forAtSign, atSign, key, NotificationType.received, opType, messageType: messageType,
             value: atValue);
         response.data = notificationId;
         return;
@@ -154,7 +154,7 @@ class NotifyVerbHandler extends AbstractVerbHandler {
     if (atConnectionMetadata.isPolAuthenticated) {
       logger.info('Storing the notification $key');
       await NotificationUtil.storeNotification(
-          fromAtSign, forAtSign, key, NotificationType.received, opType,
+          fromAtSign, forAtSign, key, NotificationType.received, opType, messageType: messageType,
           ttl_ms: ttl_ms, value: atValue);
 
       // If key is public, remove forAtSign from key.
