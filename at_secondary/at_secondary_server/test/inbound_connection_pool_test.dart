6a0004341443cd5ebc11657d4870eb76fbd7b03c
gkc <gary.casey@gmail.com>
2021-11-29 13:32:02 +0000

    inbound_connection_pool_test changes
    Changed idle time for the tests to 250 millis instead of 10000 millis and changed sleeps in the tests accordingly
    Miscellaneous code style / lint clean-up

--- at_secondary/at_secondary_server/test/inbound_connection_pool_test.dart
+++ at_secondary/at_secondary_server/test/inbound_connection_pool_test.dart
@@ -7,14 +7,16 @@ import 'package:at_secondary/src/server/server_context.dart';
 import 'package:test/test.dart';
 
 void main() async {
+  var testIdleTimeMillis = 250;
   setUp(() {
     var serverContext = AtSecondaryContext();
-    serverContext.inboundIdleTimeMillis = 10000;
+    serverContext.inboundIdleTimeMillis = testIdleTimeMillis;
     AtSecondaryServerImpl.getInstance().serverContext = serverContext;
   });
   tearDown(() {
     InboundConnectionPool.getInstance().clearAllConnections();
   });
+
   group('A group of inbound connection pool test', () {
     test('test connection pool init', () {
       InboundConnectionPool.getInstance().init((5));
@@ -25,7 +27,7 @@ void main() async {
     test('test connection pool add connections', () {
       var poolInstance = InboundConnectionPool.getInstance();
       poolInstance.init(5);
-      var dummySocket;
+      Socket? dummySocket;
       var connection1 = InboundConnectionImpl(dummySocket, 'aaa');
       var connection2 = InboundConnectionImpl(dummySocket, 'bbb');
       poolInstance.add(connection1);
@@ -36,7 +38,7 @@ void main() async {
     test('test connection pool has capacity', () {
       var poolInstance = InboundConnectionPool.getInstance();
       poolInstance.init(2);
-      var dummySocket;
+      Socket? dummySocket;
       var connection1 = InboundConnectionImpl(dummySocket, 'aaa');
       poolInstance.add(connection1);
       expect(poolInstance.hasCapacity(), true);
@@ -45,7 +47,7 @@ void main() async {
     test('test connection pool has no capacity', () {
       var poolInstance = InboundConnectionPool.getInstance();
       poolInstance.init(2);
-      var dummySocket;
+      Socket? dummySocket;
       var connection1 = InboundConnectionImpl(dummySocket, 'aaa');
       var connection2 = InboundConnectionImpl(dummySocket, 'bbb');
       poolInstance.add(connection1);
@@ -56,7 +58,7 @@ void main() async {
     test('test connection pool - clear closed connection', () {
       var poolInstance = InboundConnectionPool.getInstance();
       poolInstance.init(2);
-      var dummySocket;
+      Socket? dummySocket;
       var connection1 = MockInBoundConnectionImpl(dummySocket, 'aaa');
       var connection2 = MockInBoundConnectionImpl(dummySocket, 'bbb');
       poolInstance.add(connection1);
@@ -70,17 +72,22 @@ void main() async {
     test('test connection pool - clear idle connection', () {
       var poolInstance = InboundConnectionPool.getInstance();
       poolInstance.init(2);
-      var dummySocket;
+      Socket? dummySocket;
       var connection1 = MockInBoundConnectionImpl(dummySocket, 'aaa');
       var connection2 = MockInBoundConnectionImpl(dummySocket, 'bbb');
       var connection3 = MockInBoundConnectionImpl(dummySocket, 'ccc');
       poolInstance.add(connection1);
       poolInstance.add(connection2);
       poolInstance.add(connection3);
-      sleep(Duration(seconds: 9));
+
+      // Wait for 90 percent of the idle time to pass, then write to one of the connections
+      sleep(Duration(milliseconds: (testIdleTimeMillis * 0.9).round()));
       connection2.write('test data');
       expect(poolInstance.getCurrentSize(), 3);
-      sleep(Duration(seconds: 2));
+
+      // Now wait for the other 10 percent of the idle time to pass, plus 1 millisecond
+      // Two of the connections should now have passed their idle time, will be invalid, and will be cleared by clearInvalidConnections
+      sleep(Duration(milliseconds: (testIdleTimeMillis * 0.1).round() + 1));
       print('connection 1: ${connection1.getMetaData().created} '
           '${connection1.getMetaData().lastAccessed} ${connection1.isInValid()}');
       print('connection 2: ${connection2.getMetaData().created} '
