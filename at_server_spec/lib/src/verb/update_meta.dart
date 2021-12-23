c0cef2d756fa773ac2b34e83a2565a4ec6590261
purnimavenkatasubbu <svpurnima94@gmail.com>
2021-12-23 13:51:05 +0530

    added examples

--- at_server_spec/lib/src/verb/update_meta.dart
+++ at_server_spec/lib/src/verb/update_meta.dart
@@ -5,11 +5,18 @@ import 'package:at_commons/at_commons.dart';
 /// The @sign should be authenticated using cram verb prior to use the update meta verb.
 /// A malformed request closes the @sign client connection.
 ///
-/// Syntax: update:meta:
-/// e.g.
-/// update:meta:public:phone@alice +1 123 456 000 - update public phone number of alice
-/// update:@bob:phone@alice +1 123 456 001 - update phone number of alice shared with bob
-/// update:@alice:phone@alice + 123 456 002 - update private phone number of alice
+/// ttl - time to live
+/// Defines the time after which value should expire
+/// Accepts time duration in milliseconds
+/// update:meta:@alice:location@bob:ttl:60000 - updates the existing ttl value to 60000 for the location
+/// ttb - time to born
+/// Defines the time after which value should be displayed
+/// Accepts time duration in milliseconds
+/// update:meta:@alice:location@bob:ttb:60000 - updates the existing ttb value to 60000 for the location
+/// ttr:
+/// Creates a cached key at the receiver side.
+/// Accepts a time duration in milli seconds which is a positive integer value to refresh the cached key or -1 to cache for forever.
+/// update:meta:@alice:location@bob:ttr:60000 - updates the existing ttb value to 60000 for the location
 class UpdateMeta extends Verb {
   @override
   Verb dependsOn() => Cram();
