dc52aeeae29af12dff8712e3186e3ef2bd2ea3af
srieteja <srit1505@gmail.com>
2022-01-05 21:21:31 +0530

    new abstract class for compaction stats

--- at_secondary/at_secondary_server/lib/src/server/at_compaction_stats_impl.dart
+++ at_secondary/at_secondary_server/lib/src/server/at_compaction_stats_impl.dart
@@ -5,9 +5,9 @@ import 'package:at_persistence_spec/at_persistence_spec.dart';
 import 'package:at_commons/at_commons.dart';
 import 'package:at_secondary/src/server/at_secondary_impl.dart';
 
-class CompactionStats{
+class AtCompactionStatsImpl implements AtCompactionStats{
 
-  CompactionStats(this.atLogType);
+  AtCompactionStatsImpl(this.atLogType);
 
   AtLogType atLogType;
   DateTime? compactionStartTime;
@@ -15,19 +15,22 @@ class CompactionStats{
   int? sizeBeforeCompaction;
   int? sizeAfterCompaction;
   var keyStore = SecondaryPersistenceStoreFactory.getInstance().getSecondaryPersistenceStore(AtSecondaryServerImpl.getInstance().currentAtSign)?.getSecondaryKeyStore();
-  
+
+  @override
   void initialize(){
     sizeBeforeCompaction = atLogType.getSize();
     compactionStartTime = DateTime.now().toUtc();
   }
 
+  @override
   void calculate(){
     compactionDuration = DateTime.now().toUtc().difference(compactionStartTime!);
     sizeAfterCompaction = atLogType.getSize();
   }
 
-  void writeStats(CompactionStats compactionStats) async {
-    await keyStore?.put(commitLogCompactionKey, AtData()..data = json.encode(compactionStats));
+  @override
+  void writeStats(AtCompactionStats atCompactionStats) async {
+    await keyStore?.put(commitLogCompactionKey, AtData()..data = json.encode(atCompactionStats));
   }
 
   Map toJson()=> {
