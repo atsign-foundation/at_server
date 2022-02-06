145861b75d0f1e6d5d843cca7deb36d073489282
gkc <gary.casey@gmail.com>
2022-02-06 18:52:11 +0000

    Revert socket.destroy() in base_connection.dart.close() and just have outbound_connection_impl.dart do it (i.e. leave inbound connections behaviour unchanged)

--- at_secondary/at_secondary_server/lib/src/connection/outbound/outbound_connection_impl.dart
+++ at_secondary/at_secondary_server/lib/src/connection/outbound/outbound_connection_impl.dart
@@ -31,4 +31,32 @@ class OutboundConnectionImpl extends OutboundConnection {
   bool isInValid() {
     return _isIdle() || getMetaData().isClosed || getMetaData().isStale;
   }
+
+  @override
+  Future<void> close() async {
+    // Over-riding BaseConnection.close() (which calls socket.close()), as only want to change
+    // behaviour for outbound connections for now, not inbound connections
+
+    // Some defensive code just in case we accidentally call close multiple times
+    if (getMetaData().isClosed) {
+      return;
+    }
+
+    try {
+      var address = getSocket().remoteAddress;
+      var port = getSocket().remotePort;
+      var socket = getSocket();
+      if (socket != null) {
+        socket.destroy();
+      }
+      logger.finer('$address:$port Disconnected');
+      getMetaData().isClosed = true;
+    } on Exception {
+      getMetaData().isStale = true;
+      // Ignore exception on a connection close
+    } on Error {
+      getMetaData().isStale = true;
+      // Ignore error on a connection close
+    }
+  }
 }
