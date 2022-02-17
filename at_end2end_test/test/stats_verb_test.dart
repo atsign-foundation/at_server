b6ede521661e9e39bb4cda0d581c2e794252f6fa
purnimavenkatasubbu <svpurnima94@gmail.com>
2022-02-17 20:16:58 +0530

    fix for issue 515

--- at_end2end_test/test/stats_verb_test.dart
+++ at_end2end_test/test/stats_verb_test.dart
@@ -1,96 +1,93 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:test/test.dart';
 
-import 'commons.dart';
-import 'package:at_end2end_test/conf/config_util.dart';
+import 'e2e_test_utils.dart' as e2e;
+import 'notify_verb_test.dart' as notification;
+
 
 void main() {
-  var commitId;
-  var firstAtsign =
-      ConfigUtil.getYaml()!['first_atsign_server']['first_atsign_name'];
-  Socket? socketFirstAtsign, socketSecondAtsign;
+  late String atSign_1;
+  late e2e.SimpleOutboundSocketHandler sh1;
 
-// second atsign details
-  var secondAtsign =
-      ConfigUtil.getYaml()!['second_atsign_server']['second_atsign_name'];
+  late String atSign_2;
+  late e2e.SimpleOutboundSocketHandler sh2;
+
+  setUpAll(() async {
+    List<String> atSigns = e2e.knownAtSigns();
+    atSign_1 = atSigns[0];
+    sh1 = await e2e.getSocketHandler(atSign_1);
+    atSign_2 = atSigns[1];
+    sh2 = await e2e.getSocketHandler(atSign_2);
+  });
+
+  tearDownAll(() {
+    sh1.close();
+    sh2.close();
+  });
 
   setUp(() async {
-    var firstAtsignServer = ConfigUtil.getYaml()!['first_atsign_server']['first_atsign_url'];
-    var firstAtsignPort =
-        ConfigUtil.getYaml()!['first_atsign_server']['first_atsign_port'];
-
-    var secondAtsignServer = ConfigUtil.getYaml()!['second_atsign_server']['second_atsign_url'];
-    var secondAtsignPort =
-        ConfigUtil.getYaml()!['second_atsign_server']['second_atsign_port'];
-
-    socketFirstAtsign =
-        await secure_socket_connection(firstAtsignServer, firstAtsignPort);
-    socket_listener(socketFirstAtsign!);
-    await prepare(socketFirstAtsign!, firstAtsign);
-
-    //Socket connection for second atsign
-    socketSecondAtsign = await secure_socket_connection(
-        secondAtsignServer, secondAtsignPort);
-    socket_listener(socketSecondAtsign!);
-    await prepare(socketSecondAtsign!, secondAtsign);
+    print("Clearing socket response queues");
+    sh1.clear();
+    sh2.clear();
   });
 
   test('stats verb returns result', () async {
     /// STATS VERB
-    await socket_writer(socketFirstAtsign!, 'stats');
-    var response = await read();
-    print('stats verb response : $response');
+    await sh1.writeCommand('stats');
+    var statsResponse = await sh1.read();
+    print('stats verb response : $statsResponse');
     assert(
-        (!response.contains('Invalid syntax')) && (!response.contains('null')));
+        (!statsResponse.contains('Invalid syntax')) && (!statsResponse.contains('data:null')));
   });
 
   test('stats verb for id 3 ', () async {
     /// STATS VERB
-    await socket_writer(
-        socketFirstAtsign!, 'update:public:username$firstAtsign Bob!');
-    var response = await read();
-    print('update verb response : $response');
+    await sh1.writeCommand('update:public:username$atSign_1 Bob!');
+    var updateResponse = await sh1.read();
+    print('update verb response : $updateResponse');
     assert(
-        (!response.contains('Invalid syntax')) && (!response.contains('null')));
-    commitId = response.trim().replaceAll('data:', '');
+        (!updateResponse.contains('Invalid syntax')) && (!updateResponse.contains('null')));
+    String commitId = updateResponse.trim().replaceAll('data:', '');
     print(commitId);
 
     /// stats:3 verb response
-    await socket_writer(socketFirstAtsign!, 'stats:3');
-    response = await read();
-    print('stats verb response : $response');
-    expect(response,
+    await sh1.writeCommand('stats:3');
+    var statsResponse = await sh1.read();
+    print('stats verb response : $statsResponse');
+    expect(statsResponse,
         contains('[{"id":"3","name":"lastCommitID","value":"$commitId"'));
   });
 
   test('stats verb for invalid id ', () async {
     /// STATS VERB
-    await socket_writer(socketFirstAtsign!, 'stats:-1');
-    var response = await read();
-    print('update verb response : $response');
-    expect(response, contains('-Invalid syntax'));
+    await sh1.writeCommand('stats:-1');
+    var statsResponse = await sh1.read();
+    print('stats verb esponse : $statsResponse');
+    expect(statsResponse, contains('Invalid syntax'));
+    // As invalid syntx closes the connection. Creating a new connection
+    sh1.close();
+    sh1 = await e2e.getSocketHandler(atSign_1);
   });
+
 
   test(
       'stats verb for id 11 - update operation count from sender for the messageType key',
       () async {
     /// stats:11 verb response
-    var beforeUpdate = await notificationStats(socketFirstAtsign!);
+    var beforeUpdate = await notificationStats(sh1);
     var sentCountBeforeUpdate = await beforeUpdate['type']['sent'];
     var statusBeforeUpdate = await beforeUpdate['status']['delivered'];
     var keyCountBeforeUpdate = await beforeUpdate['messageType']['key'];
 
-    /// update command
-    await socket_writer(socketFirstAtsign!,
-        'update:$secondAtsign:country$firstAtsign India');
-    var updateResponse = await read();
-    print('update verb response $updateResponse');
-    assert((!updateResponse.contains('Invalid syntax')) &&
-        (!updateResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterUpdate = await notificationStats(socketFirstAtsign!);
+    /// notify command
+    await sh1.writeCommand('notify:update:ttr:-1:$atSign_2:country$atSign_1:India');
+    var notifyResponse = await sh1.read();
+    print('notify verb response $notifyResponse');
+    assert((!notifyResponse.contains('Invalid syntax')) && (!notifyResponse.contains('null')));
+    String notificationId = notifyResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['delivered'], timeOutMillis: 15000);
+    var afterUpdate = await notificationStats(sh1);
     var sentCountAfterUpdate = await afterUpdate['type']['sent'];
     var statusAfterUpdate = await afterUpdate['status']['delivered'];
     var keyCountAfterUpdate = await afterUpdate['messageType']['key'];
@@ -105,18 +102,17 @@ void main() {
       'stats verb for id 11 - update operation count from receiver for the messageType key',
       () async {
     /// stats:11 verb response
-    var beforeUpdateCount = await notificationStats(socketSecondAtsign!);
+    var beforeUpdateCount = await notificationStats(sh2);
     var receivedCountBeforeUpdate = beforeUpdateCount['type']['received'];
 
     /// update command
-    await socket_writer(socketFirstAtsign!,
-        'update:$secondAtsign:country$firstAtsign India');
-    var updateResponse = await read();
-    print('update verb response $updateResponse');
-    assert((!updateResponse.contains('Invalid syntax')) &&
-        (!updateResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterUpdateCount = await notificationStats(socketSecondAtsign!);
+    await sh1.writeCommand('notify:update:$atSign_2:country$atSign_1:India');
+    var notifyResponse = await sh1.read();
+    print('notify verb response $notifyResponse');
+    assert((!notifyResponse.contains('Invalid syntax')) && (!notifyResponse.contains('null')));
+    String notificationId = notifyResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['delivered'], timeOutMillis: 15000);
+    var afterUpdateCount = await notificationStats(sh2);
     var receivedCountAfterUpdate = afterUpdateCount['type']['received'];
     expect(afterUpdateCount['operations']['update'],
         beforeUpdateCount['operations']['update'] + 1);
@@ -127,19 +123,19 @@ void main() {
       'stats verb for id 11 - delete operation count from sender for the messageType key',
       () async {
     /// stats:11 verb response
-    var beforeDelete = await notificationStats(socketFirstAtsign!);
+    var beforeDelete = await notificationStats(sh1);
     var sentCountBeforeDelete = await beforeDelete['type']['sent'];
     var statusBeforeDelete = await beforeDelete['status']['delivered'];
 
     /// delete command
-    await socket_writer(
-        socketFirstAtsign!, 'delete:$secondAtsign:country$firstAtsign');
-    var deleteResponse = await read();
-    print('delete verb response $deleteResponse');
+    await sh1.writeCommand('notify:delete:$atSign_2:country$atSign_1');
+    var deleteResponse = await sh1.read();
+    print(' notify delete verb response $deleteResponse');
     assert((!deleteResponse.contains('Invalid syntax')) &&
         (!deleteResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterDelete = await notificationStats(socketFirstAtsign!);
+    String notificationId = deleteResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['delivered'], timeOutMillis: 15000);
+    var afterDelete = await notificationStats(sh1);
     var sentCountAfterDelete = await afterDelete['type']['sent'];
     var statusAfterDelete = await afterDelete['status']['delivered'];
     expect(afterDelete['operations']['delete'],
@@ -148,20 +144,20 @@ void main() {
     expect(statusAfterDelete, statusBeforeDelete + 1);
   });
 
-  test('stats verb for id 11 - delete operation count from receiver', () async {
+   test('stats verb for id 11 - delete operation count from receiver', () async {
     /// stats:11 verb response
-    var beforeDelete = await notificationStats(socketSecondAtsign!);
+    var beforeDelete = await notificationStats(sh2);
     var sentCountBeforeDelete = await beforeDelete['type']['received'];
 
     /// delete command
-    await socket_writer(
-        socketFirstAtsign!, 'delete:$secondAtsign:country$firstAtsign');
-    var deleteResponse = await read();
-    print('delete verb response $deleteResponse');
+    await sh1.writeCommand('notify:delete:$atSign_2:country$atSign_1');
+    var deleteResponse = await sh1.read();
+    print('notify delete verb response $deleteResponse');
     assert((!deleteResponse.contains('Invalid syntax')) &&
         (!deleteResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterDelete = await notificationStats(socketSecondAtsign!);
+    String notificationId = deleteResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['delivered'], timeOutMillis: 15000);
+    var afterDelete = await notificationStats(sh2);
     var sentCountAfterDelete = await afterDelete['type']['received'];
     expect(afterDelete['operations']['delete'],
         beforeDelete['operations']['delete'] + 1);
@@ -170,20 +166,20 @@ void main() {
 
   test('stats verb for id 11 - for messageType text ', () async {
     /// stats:11 verb response
-    var beforeNotify = await notificationStats(socketFirstAtsign!);
+    var beforeNotify = await notificationStats(sh1);
     var sentCountBeforeNotify = await beforeNotify['type']['sent'];
     var statusBeforeNotify = await beforeNotify['status']['delivered'];
     var textCountBeforeNotify = await beforeNotify['messageType']['text'];
 
     /// update command
-    await socket_writer(socketFirstAtsign!,
-        'notify:messageType:text:ttr:-1:$secondAtsign:message$firstAtsign:Hi!!!');
-    var notifyResponse = await read();
+    await sh1.writeCommand('notify:messageType:text:ttr:-1:$atSign_2:message$atSign_1:Hi!!!');
+    var notifyResponse = await sh1.read();
     print('notify verb response $notifyResponse');
     assert((!notifyResponse.contains('Invalid syntax')) &&
         (!notifyResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterNotify = await notificationStats(socketFirstAtsign!);
+    String notificationId = notifyResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['delivered'], timeOutMillis: 15000);
+    var afterNotify = await notificationStats(sh1);
     var sentCountAfterNotify = await afterNotify['type']['sent'];
     var statusAfterNotify = await afterNotify['status']['delivered'];
     var textCountAfterNotify = await afterNotify['messageType']['text'];
@@ -194,20 +190,20 @@ void main() {
 
   test('stats verb for id 11 - for an invalid atsign ', () async {
     /// stats:11 verb response
-    var beforeUpdate = await notificationStats(socketFirstAtsign!);
+    var beforeUpdate = await notificationStats(sh1);
     var sentCountBeforeUpdate = await beforeUpdate['type']['sent'];
     var statusBeforeUpdate = await beforeUpdate['status']['failed'];
     var keyCountBeforeUpdate = await beforeUpdate['messageType']['key'];
 
     /// update command
-    await socket_writer(
-        socketFirstAtsign!, 'update:@xxx:country$firstAtsign India');
-    var updateResponse = await read();
-    print('update verb response $updateResponse');
-    assert((!updateResponse.contains('Invalid syntax')) &&
-        (!updateResponse.contains('null')));
-    await Future.delayed(Duration(seconds: 16));
-    var afterUpdate = await notificationStats(socketFirstAtsign!);
+    await sh1.writeCommand('notify:update:@xxx:country$atSign_1:India');
+    var notifyResponse = await sh1.read();
+    print('notify verb response $notifyResponse');
+    assert((!notifyResponse.contains('Invalid syntax')) &&
+        (!notifyResponse.contains('null')));
+    String notificationId = notifyResponse.replaceAll('data:', '');
+    await notification.getNotifyStatus(sh1, notificationId, returnWhenStatusIn: ['errored']);
+    var afterUpdate = await notificationStats(sh1);
     var sentCountAfterUpdate = await afterUpdate['type']['sent'];
     var statusAfterUpdate = await afterUpdate['status']['failed'];
     var keyCountAfterUpdate = await afterUpdate['messageType']['key'];
@@ -218,17 +214,11 @@ void main() {
     expect(keyCountAfterUpdate, keyCountBeforeUpdate + 1);
   });
 
-
-  tearDown(() {
-    //Closing the client socket connection
-    clear();
-    socketFirstAtsign!.destroy();
-  });
 }
 
-Future<Map> notificationStats(Socket socket) async {
-  await socket_writer(socket, 'stats:11');
-  var statsResponse = await read();
+Future<Map> notificationStats(e2e.SimpleOutboundSocketHandler sh) async {
+  await sh.writeCommand('stats:11');
+  var statsResponse = await sh.read();
   print('stats verb response : $statsResponse');
   var jsonData =
       jsonDecode(statsResponse.replaceAll('data:', '').trim().toString());
