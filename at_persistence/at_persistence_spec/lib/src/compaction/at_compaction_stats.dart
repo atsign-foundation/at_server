23178969dd0b01a461c99cf4a930f06e3f10c585
srieteja <srit1505@gmail.com>
2022-02-18 01:55:42 +0530

    at_compaction_stats attributes made nullable

--- at_persistence/at_persistence_spec/lib/src/compaction/at_compaction_stats.dart
+++ at_persistence/at_persistence_spec/lib/src/compaction/at_compaction_stats.dart
@@ -2,25 +2,28 @@ import 'package:at_persistence_spec/at_persistence_spec.dart';
 
 ///Type to collect and store compaction statistics
 class AtCompactionStats {
-  late CompactionType compactionType;
-  late DateTime lastCompactionRun;
-  late Duration compactionDuration;
-  late int sizeBeforeCompaction;
-  late int sizeAfterCompaction;
-  late int deletedKeysCount;
+  CompactionType? compactionType;
+  late DateTime? lastCompactionRun;
+  late Duration? compactionDuration;
+  int? sizeBeforeCompaction;
+  int? sizeAfterCompaction;
+  int? deletedKeysCount;
+
+  AtCompactionStats();
 
   ///maps predefined keys to their values which will be ready to encode to json
   Map toJson() => {
-        'compaction_type': compactionType.toString(),
-        'last_compaction_run': lastCompactionRun.toString(),
-        'duration': compactionDuration.toString(),
-        'size_before_compaction': sizeBeforeCompaction.toString(),
-        'size_after_compaction': sizeAfterCompaction.toString(),
-        'deleted_keys_count': deletedKeysCount.toString()
+        'compaction_type': compactionType?.toString()  ,
+        'last_compaction_run': lastCompactionRun?.toString(),
+        'duration': compactionDuration?.toString(),
+        'size_before_compaction': sizeBeforeCompaction?.toString(),
+        'size_after_compaction': sizeAfterCompaction?.toString(),
+        'deleted_keys_count': deletedKeysCount?.toString()
       };
 
   @override
   String toString() {
-    return "compaction type: $compactionType, compaction last run at: $lastCompactionRun, compaction duration: $compactionDuration, size before compaction: $sizeBeforeCompaction, size after compaction: $sizeAfterCompaction, no. of keys deleted: $deletedKeysCount";
+    return toJson().toString();
+    //return "compaction type: $compactionType, compaction last run at: $lastCompactionRun, compaction duration: $compactionDuration, size before compaction: $sizeBeforeCompaction, size after compaction: $sizeAfterCompaction, no. of keys deleted: $deletedKeysCount";
   }
 }
