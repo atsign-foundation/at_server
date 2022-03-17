62aeeb3188a18795c726571bf99cc2857d28b6c1
gkc <gary.casey@gmail.com>
2022-03-17 09:40:21 +0000

    I think the features need an 'id' as well as a 'name'

--- at_secondary/at_secondary_server/lib/src/verb/handler/info_verb_handler.dart
+++ at_secondary/at_secondary_server/lib/src/verb/handler/info_verb_handler.dart
@@ -23,27 +23,7 @@ class InfoVerbHandler extends AbstractVerbHandler {
   @override
   Future<void> processVerb(Response response, HashMap<String, String?> verbParams, InboundConnection atConnection) async {
     Map infoMap = {}; // structure of what is returned is documented in the [Info] verb in at_server_spec
-    /// The "info" verb returns a JSON object as follows:
-    /// ```json
-    /// {
-    ///   "version" : "the version being run",
-    ///   "uptime" : "uptime as string: D days, H hours, M minutes, S seconds",
-    ///   "features" : [
-    ///     {
-    ///       "name" : "name of feature 1",
-    ///       "status" : "One of Preview, Beta, GA",
-    ///       "description" : "optional description of feature"
-    ///     },
-    ///     {
-    ///       "name" : "name of feature 2",
-    ///       "status" : "One of Preview, Beta, GA",
-    ///       "description" : "optional description of feature"
-    ///     },
-    ///     ...
-    ///   ]
-    /// }
-    /// ```
-    ///
+
     infoMap['version'] = AtSecondaryConfig.secondaryServerVersion;
     Duration uptime = Duration(milliseconds: DateTime.now().millisecondsSinceEpoch - approximateStartTimeMillis!);
     int uDays = uptime.inDays;
