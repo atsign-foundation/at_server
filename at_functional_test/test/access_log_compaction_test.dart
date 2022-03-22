a1f6f6d80b0c682179646d9fb60717d8d99cdb78
purnimavenkatasubbu <svpurnima94@gmail.com>
2022-03-22 14:56:50 +0530

    added access log compaction tests

--- at_functional_test/test/access_log_compaction_test.dart
+++ at_functional_test/test/access_log_compaction_test.dart
@@ -22,14 +22,14 @@ void main() {
     await prepare(socketFirstAtsign!, firstAtsign);
   });
 
-  test('Test that stats:12 returns the commit Log stats', () async {
-    await socket_writer(socketFirstAtsign!, 'stats:12');
+  test('Test that stats:13 returns the access Log stats', () async {
+    await socket_writer(socketFirstAtsign!, 'stats:13');
     var response = await read();
-    print("stats:12 response is $response");
-    assert(response.contains('"name":"CommitLogCompactionStats"'));
+    print("stats:13 response is $response");
+    assert(response.contains('"name":"AccessLogCompactionStats"'));
   });
 
-  test('commit log compaction', () async {
+  test('Access log compaction', () async {
     int noOfTests = 30;
     for (int i = 1; i <= noOfTests; i++) {
       await socket_writer(
@@ -41,7 +41,7 @@ void main() {
     }
 
     await Future.delayed(Duration(seconds: 50));
-    var afterUpdate = await  compactionStats(socketFirstAtsign!, 12);
+    var afterUpdate = await  compactionStats(socketFirstAtsign!, 13);
     var preCompactionCount = afterUpdate['preCompactionEntriesCount'];
     var postCompactionCount = await afterUpdate['postCompactionEntriesCount'];
     var deletedCountAfter = await afterUpdate['deletedKeysCount'];
@@ -51,36 +51,6 @@ void main() {
     expect((postCompactionCount != preCompactionCount), true);
     int result = (int.parse(preCompactionCount) - (int.parse(postCompactionCount)));
     expect(int.parse(deletedCountAfter),result );
-  }, timeout: Timeout(Duration(seconds: 150)));
-
-  test('access log compaction', () async {
-    int randomNumber = Random().nextInt(30);
-    var beforeUpdate = await compactionStats(socketFirstAtsign!, 13);
-    var sizeBeforeCompaction = await beforeUpdate['size_before_compaction'];
-    var deletedCountBefore = await beforeUpdate['deletedKeysCount'];
-    var lastRunTime = await beforeUpdate['last_compaction_run'];
-    print('size before compaction is $sizeBeforeCompaction');
-    print('deleted keys before update is $deletedCountBefore');
-    print('last compaction run time is $lastRunTime');
-
-    int noOfTests = 30;
-    for (int i = 1; i <= noOfTests; i++) {
-      await socket_writer(socketFirstAtsign!,
-          'update:public:pin$firstAtsign 1122$randomNumber');
-      var response = await read();
-      print('update verb response : $response');
-      assert((!response.contains('Invalid syntax')) &&
-          (!response.contains('null')));
-    }
-
-    await Future.delayed(Duration(seconds: 40));
-    var afterUpdate = await compactionStats(socketFirstAtsign!, 13);
-    var sizeAfterCompaction = await afterUpdate['size_after_compaction'];
-    var deletedCountAfter = await afterUpdate['deletedKeysCount'];
-    print('deleted keys count after update is $deletedCountAfter');
-    print('size after compaction is $sizeAfterCompaction');
-    expect(sizeAfterCompaction, 0);
-    expect(deletedCountAfter, (noOfTests - 1));
   }, timeout: Timeout(Duration(seconds: 150)));
 
   tearDown(() {
