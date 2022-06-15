e42d2e86e72c3b9ad256456cd5109485c794a513
srieteja <srit1505@gmail.com>
2022-06-15 16:24:50 +0530

    at_persistence_spec part of at_annotation

---
+++ at_persistence/at_persistence_spec/lib/src/annotation/at_annotation.dart
@@ -0,0 +1,14 @@
+///Class for at_server custom annotation.
+///Methods that are used by at_secondary_server are marked with this annotation.
+class AtServerAnnotation {
+  const AtServerAnnotation();
+}
+
+///Class for at_client annotation.
+///Methods that are used by at_client/at_client_mobile are marked with this annotation.
+class AtClientAnnotation {
+  const AtClientAnnotation();
+}
+
+const AtServerAnnotation server = AtServerAnnotation();
+const AtClientAnnotation client = AtClientAnnotation();
