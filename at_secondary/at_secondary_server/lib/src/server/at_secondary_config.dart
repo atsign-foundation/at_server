33c2ad1d3c2319508c3ac2369806f7955f8692b7
Srie Teja <srit1505@gmail.com>
2022-06-23 20:08:14 +0530

    setting env vars

--- at_secondary/at_secondary_server/lib/src/server/at_secondary_config.dart
+++ at_secondary/at_secondary_server/lib/src/server/at_secondary_config.dart
@@ -120,15 +120,10 @@ class AtSecondaryConfig {
   }
 
   static bool get testingMode {
+    bool? result = Platform.environment['testingMode'] == 'true' ? true : false;
+    return result;
     try {
-      bool? result = _getBoolEnvVar('testingMode');
-      if (result != null) {
-        return result;
-      }
-    } on Exception {}
-
-    try {
-        return getConfigFromYaml(['testing', 'testingMode']);
+      return getConfigFromYaml(['testing', 'testingMode']);
     } on ElementNotFoundException {
       return _testingMode;
     }
