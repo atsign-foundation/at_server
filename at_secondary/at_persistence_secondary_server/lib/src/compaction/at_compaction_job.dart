8eae10809de1e67ce5a7d20ac53ccf11b4905b85
srieteja <50960424+srieteja@users.noreply.github.com>
2022-06-24 16:54:12 +0530

    introduce stop compaction job in at_compaction_job

--- at_secondary/at_persistence_secondary_server/lib/src/compaction/at_compaction_job.dart
+++ at_secondary/at_persistence_secondary_server/lib/src/compaction/at_compaction_job.dart
@@ -6,19 +6,25 @@ import 'package:cron/cron.dart';
 class AtCompactionJob {
   late Cron _cron;
   AtLogType atLogType;
+  late ScheduledTask _schedule;
   //instance of SecondaryPersistenceStore stored to be passed on to AtCompactionStatsImpl
-  late SecondaryPersistenceStore _secondaryPersistenceStore;
+  late final SecondaryPersistenceStore _secondaryPersistenceStore;
 
   AtCompactionJob(this.atLogType, this._secondaryPersistenceStore);
 
   void scheduleCompactionJob(AtCompactionConfig atCompactionConfig) {
     var runFrequencyMins = atCompactionConfig.compactionFrequencyMins;
     _cron = Cron();
-    _cron.schedule(Schedule.parse('*/$runFrequencyMins * * * *'), () async {
+    _schedule =
+        _cron.schedule(Schedule.parse('*/$runFrequencyMins * * * *'), () async {
       var compactionService = AtCompactionService.getInstance();
       compactionService.executeCompaction(
           atCompactionConfig, atLogType, _secondaryPersistenceStore);
     });
+  }
+  
+  Future<void> stopCompactionJob() async {
+    await _schedule.cancel();
   }
 
   void close() {
