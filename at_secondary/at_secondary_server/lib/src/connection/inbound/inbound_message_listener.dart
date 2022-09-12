fce3e4e19402b1d4290790744d868285218be231
srieteja <50960424+srieteja@users.noreply.github.com>
2022-09-13 00:54:37 +0530

    Update inbound_message_listener.dart

--- at_secondary/at_secondary_server/lib/src/connection/inbound/inbound_message_listener.dart
+++ at_secondary/at_secondary_server/lib/src/connection/inbound/inbound_message_listener.dart
@@ -39,6 +39,7 @@ class InboundMessageListener {
   Future<void> _messageHandler(data) async {
     //ignore the data read if the connection is stale or closed
     if (connection.getMetaData().isStale || connection.getMetaData().isClosed){
+      //clear buffer as data is redundant
       _buffer.clear();
       //close if a stale connection is still open as stale connections are not useful
       if (!connection.getMetaData().isClosed){
