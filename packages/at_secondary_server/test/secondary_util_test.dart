5912df1d4cea5002487b1af4ed4c43577b18bf99
srieteja <srit1505@gmail.com>
2022-11-28 20:52:45 +0530

    revert original implementation

--- packages/at_secondary_server/test/secondary_util_test.dart
+++ packages/at_secondary_server/test/secondary_util_test.dart
@@ -3,28 +3,9 @@ import 'package:test/test.dart';
 
 void main() {
   group('group of command conversion test', () {
-    test('convert command with value', () {
+    test('convert command', () {
       var result = SecondaryUtil.convertCommand('update:privateKey:abc HelLo');
       print(result);
-      expect(result, 'update:privatekey:abc HelLo');
     });
-
-    test('convert command with value - verb has upper case', () {
-      var result = SecondaryUtil.convertCommand('UpdAte:Phone:aBc HelLo');
-      print(result);
-      expect(result, 'update:phone:abc HelLo');
-    });
-
-    test('convert command without value', () {
-      var result = SecondaryUtil.convertCommand('delEte:privateKey:aBc');
-      print(result);
-      expect(result, 'delete:privatekey:abc');
-    });
-
-    test('convert command without \':\'', (){
-      var result = SecondaryUtil.convertCommand('scAn');
-      expect(result, 'scan');
-    });
-
   });
 }
