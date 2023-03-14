bd075f05d33f1ba13e900c4db4aa567afd5f8175
Sitaram Kalluri <kalluriramkumar@gmail.com>
2023-03-14 21:51:12 +0530

    fix: check root server for readiness

--- tests/at_functional_test/test/check_root_server_readiness.dart
+++ tests/at_functional_test/test/check_root_server_readiness.dart
@@ -4,48 +4,38 @@ import 'dart:io';
 
 import 'package:test/test.dart';
 
-var maxRetryCount = 100;
+var maxRetryCount = 50;
 var retryCount = 1;
 Queue rootServerResponseQueue = Queue();
 
 void main() {
-  var atsign = 'sitaram🛠';
   var rootServerPort = 64;
   var rootServer = 'vip.ve.atsign.zone';
 
   late SecureSocket _secureSocket;
 
-  test('checking for test environment readiness', () async {
+  test('checking for root server readiness', () async {
     while (retryCount < maxRetryCount) {
       try {
         _secureSocket = await SecureSocket.connect(rootServer, rootServerPort,
             timeout: Duration(seconds: 1));
         socketListener(_secureSocket);
-        while (rootServerResponseQueue.isEmpty) {
-          _secureSocket.write('$atsign\n');
-          await Future.delayed(Duration(milliseconds: 10));
-          if (rootServerResponseQueue.isNotEmpty) {
-            var rootResponse = rootServerResponseQueue.removeFirst();
-            print('Root server started: $rootResponse');
-            await _secureSocket.close();
-            break;
-          } else {
-            print(
-                'Waiting for the root server to start: RetryCount: $retryCount');
-          }
+        var response = await readResponse();
+        if (response == '@') {
+          print('Root Server is up and running');
+          break;
         }
       } on SocketException {
         print('Waiting for the root server to start: RetryCount: $retryCount');
-        await Future.delayed(Duration(seconds: 5));
+        await Future.delayed(Duration(seconds: 2));
         retryCount = retryCount + 1;
       } on HandshakeException {
         print('Waiting for the root server to start: RetryCount: $retryCount');
-        await Future.delayed(Duration(seconds: 5));
+        await Future.delayed(Duration(seconds: 2));
         retryCount = retryCount + 1;
       }
     }
-
-  }, timeout: Timeout(Duration(minutes: 1)));
+  });
 }
 
 void socketListener(SecureSocket secureSocket) {
@@ -56,4 +46,11 @@ void socketListener(SecureSocket secureSocket) {
       rootServerResponseQueue.add(response);
     }
   });
+}
+
+dynamic readResponse() async {
+  while (rootServerResponseQueue.isEmpty) {
+    await Future.delayed(Duration(milliseconds: 10));
+  }
+  return rootServerResponseQueue.removeFirst();
 }
