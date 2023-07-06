7945ce61bb6179f2ad7615058ada2d7b3e55e78e
murali-shris <murali.25@gmail.com>
2023-07-06 22:21:57 +0530

    fix: uncommented info verb test and added version check

--- tests/at_functional_test/test/info_verb_test.dart
+++ tests/at_functional_test/test/info_verb_test.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 
 import 'package:at_functional_test/conf/config_util.dart';
 import 'package:test/test.dart';
+import 'package:version/version.dart';
 
 import 'at_demo_data.dart';
 import 'functional_test_commons.dart';
@@ -23,46 +24,48 @@ void main() {
         ConfigUtil.getYaml()!['first_atsign_server']['first_atsign_port'];
 
     socketFirstAtsign =
-        await secure_socket_connection(firstAtsignServer, firstAtsignPort);
+        await socketConnection(firstAtsignServer, firstAtsignPort);
     socket_listener(socketFirstAtsign!);
   });
 
   test('info verb test without authentication', () async {
     await socket_writer(socketFirstAtsign!, 'info');
     var infoVerbResponse = await read();
-    print('info verb response : $infoVerbResponse');
     infoVerbResponse = infoVerbResponse.replaceAll('data:', '');
     var infoResponse = jsonDecode(infoVerbResponse);
     expect(infoResponse['version'], isNotEmpty);
   });
 
   // commenting the test as the server doesn't have enroll verb changes yet
-  // test('info verb with enroll verb changes', () async {
-  //   await socket_writer(socketFirstAtsign!, 'from:$firstAtsign');
-  //   var fromResponse = await read();
-  //   print('from verb response : $fromResponse');
-  //   fromResponse = fromResponse.replaceAll('data:', '');
-  //   var pkamDigest = generatePKAMDigest(firstAtsign, fromResponse);
-  //   await socket_writer(socketFirstAtsign!, 'pkam:$pkamDigest');
-  //   var pkamResult = await read();
-  //   expect(pkamResult, 'data:success\n');
+  test('info verb with enroll verb changes', () async {
+    await socket_writer(socketFirstAtsign!, 'info:brief');
+    var infoResponse = await read();
+    infoResponse = infoResponse.replaceFirst('data:', '');
+    var serverVersion = jsonDecode(infoResponse)['version'];
+    if (Version.parse(serverVersion) > Version(3, 0, 34)) {
+      await socket_writer(socketFirstAtsign!, 'from:$firstAtsign');
+      var fromResponse = await read();
+      fromResponse = fromResponse.replaceAll('data:', '');
+      var pkamDigest = generatePKAMDigest(firstAtsign, fromResponse);
+      await socket_writer(socketFirstAtsign!, 'pkam:$pkamDigest');
+      var pkamResult = await read();
+      expect(pkamResult, 'data:success\n');
 
-  //   // create a key with the _manage namespace
-  //   var enrollRequest =
-  //       'enroll:request:appName:wavi:deviceName:pixel:namespaces:[wavi,rw]:apkamPublicKey:${pkamPublicKeyMap[firstAtsign]!}\n';
-  //   await socket_writer(socketFirstAtsign!, enrollRequest);
-  //   var enrollResponse = await read();
-  //   print(enrollResponse);
-  //   enrollResponse = enrollResponse.replaceFirst('data:', '');
-  //   var enrollJsonMap = jsonDecode(enrollResponse);
-  //   expect(enrollJsonMap['enrollmentId'], isNotEmpty);
+      // create a key with the _manage namespace
+      var enrollRequest =
+          'enroll:request:appName:wavi:deviceName:pixel:namespaces:[wavi,rw]:apkamPublicKey:${pkamPublicKeyMap[firstAtsign]!}\n';
+      await socket_writer(socketFirstAtsign!, enrollRequest);
+      var enrollResponse = await read();
+      enrollResponse = enrollResponse.replaceFirst('data:', '');
+      var enrollJsonMap = jsonDecode(enrollResponse);
+      expect(enrollJsonMap['enrollmentId'], isNotEmpty);
 
-  //   // check the info verb.. It should return the result
-  //   await socket_writer(socketFirstAtsign!, 'info');
-  //   var infoVerbResponse = await read();
-  //   print('info verb response : $infoVerbResponse');
-  //   infoVerbResponse = infoVerbResponse.replaceAll('data:', '');
-  //   var infoResponse = jsonDecode(infoVerbResponse);
-  //   expect(infoResponse['apkam_metadata'], isNotEmpty);
-  // });
+      // check the info verb.. It should return the result
+      await socket_writer(socketFirstAtsign!, 'info');
+      var infoVerbResponse = await read();
+      infoVerbResponse = infoVerbResponse.replaceAll('data:', '');
+      var infoResponse = jsonDecode(infoVerbResponse);
+      expect(infoResponse['apkam_metadata'], isNotEmpty);
+    }
+  });
 }
