1bf8e28f2a9d7a689061263b85aa5db4f9c509bc
purnimavenkatasubbu <svpurnima94@gmail.com>
2023-07-06 16:28:57 +0530

    info verb changes

--- packages/at_secondary_server/lib/src/verb/handler/info_verb_handler.dart
+++ packages/at_secondary_server/lib/src/verb/handler/info_verb_handler.dart
@@ -1,16 +1,22 @@
 import 'dart:collection';
 import 'dart:convert';
+
 import 'package:at_commons/at_commons.dart';
-import 'package:at_secondary/src/server/at_secondary_config.dart';
-import 'package:at_server_spec/at_server_spec.dart';
-import 'abstract_verb_handler.dart';
-import 'package:at_server_spec/at_verb_spec.dart';
 import 'package:at_persistence_secondary_server/at_persistence_secondary_server.dart';
+import 'package:at_secondary/src/connection/inbound/inbound_connection_metadata.dart';
+import 'package:at_secondary/src/constants/key_constants.dart';
+import 'package:at_secondary/src/server/at_secondary_config.dart';
+import 'package:at_secondary/src/server/at_secondary_impl.dart';
+import 'package:at_server_spec/at_server_spec.dart';
+import 'package:at_server_spec/at_verb_spec.dart';
+
+import 'abstract_verb_handler.dart';
 
 /// Handler for the 'info' verb. Usage of info verb is documented in at_server_spec/lib/src/verb/info.dart
 class InfoVerbHandler extends AbstractVerbHandler {
   static Info infoVerb = Info();
   static int? approximateStartTimeMillis;
+
   InfoVerbHandler(SecondaryKeyStore keyStore) : super(keyStore) {
     approximateStartTimeMillis ??= DateTime.now().millisecondsSinceEpoch;
   }
@@ -26,8 +32,15 @@ class InfoVerbHandler extends AbstractVerbHandler {
       Response response,
       HashMap<String, String?> verbParams,
       InboundConnection atConnection) async {
-    Map infoMap =
-        {}; // structure of what is returned is documented in the [Info] verb in at_server_spec
+    Map infoMap = {};
+    String? apkamMetadataKey;
+    String? result;
+    InboundConnectionMetadata atConnectionMetadata = atConnection.getMetaData()
+        as InboundConnectionMetadata; // structure of what is returned is documented in the [Info] verb in at_server_spec
+    var atSign = AtSecondaryServerImpl.getInstance().currentAtSign;
+    final enrollApprovalId = atConnectionMetadata.enrollApprovalId;
+    apkamMetadataKey =
+        '$enrollApprovalId.$newEnrollmentKeyPattern.$enrollManageNamespace$atSign';
 
     infoMap['version'] = AtSecondaryConfig.secondaryServerVersion;
     Duration uptime = Duration(
@@ -36,6 +49,12 @@ class InfoVerbHandler extends AbstractVerbHandler {
     if (verbParams[paramFullCommandAsReceived] == 'info') {
       String uptimeAsWords = durationToWords(uptime);
       infoMap['uptimeAsWords'] = uptimeAsWords;
+      if (atConnectionMetadata.isAuthenticated) {
+        result = await _getapkamMetadataKey(apkamMetadataKey);
+        if (result != null) {
+          infoMap['apkam_metadata'] = result;
+        }
+      }
       infoMap['features'] = [
         {
           "name": "noop:",
@@ -53,7 +72,7 @@ class InfoVerbHandler extends AbstractVerbHandler {
               "The Info verb returns some information about the server "
                   "including uptime and some info about available features. ",
           "syntax": VerbSyntax.info
-        }
+        },
       ];
     } else {
       infoMap['uptimeAsMillis'] = uptime.inMilliseconds;
@@ -72,5 +91,15 @@ class InfoVerbHandler extends AbstractVerbHandler {
         ((uDays > 0 || uHours > 0 || uMins > 0) ? "$uMins minutes " : "") +
         "$uSeconds seconds";
     return uptimeAsWords;
+  }
+
+  Future<String?> _getapkamMetadataKey(String? apkamMetadataKey) async {
+    AtData? result;
+    try {
+      result = await keyStore.get(apkamMetadataKey);
+    } on Exception catch (e) {
+      logger.severe('apkam_metadata not found | $e');
+    }
+    return result?.data;
   }
 }
