5bfc76aff9f5ab2e73c9f967d26c0cb10da095bc
Srie Teja <srit1505@gmail.com>
2023-09-14 14:07:50 +0530

    feat: introduce new key format for supplementary enrollments

--- packages/at_secondary_server/lib/src/constants/enroll_constants.dart
+++ packages/at_secondary_server/lib/src/constants/enroll_constants.dart
@@ -1,5 +1,6 @@
 const String enrollManageNamespace = '__manage';
 const String newEnrollmentKeyPattern = 'new.enrollments';
+const String updateEnrollmentKeyPattern = 'supplementary.enrollments';
 const String pkamNamespace = '__pkams';
 const String globalNamespace = '__global';
 const String allNamespaces = '*';
