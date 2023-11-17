d93d3cde216a6487e8839ab44c339a84856fcc22
murali-shris <murali.25@gmail.com>
2023-11-17 17:11:52 +0530

    feat: new demo atsigns for apkam

--- tools/build_virtual_environment/install_PKAM_Keys/bin/install_PKAM_Keys.dart
+++ tools/build_virtual_environment/install_PKAM_Keys/bin/install_PKAM_Keys.dart
@@ -4,41 +4,60 @@ import 'package:at_lookup/at_lookup.dart';
 void main(List<String> arguments) async {
   await Future.forEach(at_demo_data.allAtsigns, (String atSign) async {
     if (atSign != 'anonymous') {
-      await lookItUp(atSign);
+      await cramAuthAndUpdateKeys(atSign);
     }
+  });
+  await Future.forEach(at_demo_data.apkamAtsigns, (String atSign) async {
+    await cramAuth(atSign);
   });
 }
 
-Future<void> lookItUp(String atSign) async {
+Future<void> cramAuth(String atSign) async {
   try {
     var _atLookup = AtLookupImpl(atSign, 'vip.ve.atsign.zone', 64);
     var isCramAuthSuccessful =
-        await _atLookup.authenticate_cram(at_demo_data.cramKeyMap[atSign]);
+        await _atLookup.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
+    if (!isCramAuthSuccessful) {
+      print('CRAM Authentication failed for $atSign');
+      return;
+    }
+    print('CRAM Authentication is successful for $atSign');
+    await _atLookup.close();
+  } on Exception catch (e) {
+    print('error while cram auth for ${atSign} exception: ${e.toString()}');
+  }
+}
+
+Future<void> cramAuthAndUpdateKeys(String atSign) async {
+  try {
+    var _atLookup = AtLookupImpl(atSign, 'vip.ve.atsign.zone', 64);
+    var isCramAuthSuccessful =
+        await _atLookup.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
     if (!isCramAuthSuccessful) {
       print('CRAM Authentication failed for $atSign');
       return;
     }
     print('CRAM Authentication is successful for $atSign');
 
-    // Set PKAM private key
+    // Set PKAM public key
     var command =
         'update:privatekey:at_pkam_publickey ${at_demo_data.pkamPublicKeyMap[atSign]}\n';
     var response = await _atLookup.executeCommand(command, auth: true);
     if (response == 'data:-1') {
-      print('Setting of PKAM private key for $atSign is successful');
+      print('Setting of PKAM public key for $atSign is successful');
     } else {
-      print('Failed to update PKAM private key for $atSign');
+      print('Failed to update PKAM public key for $atSign');
       return;
     }
 
-    // Set PKAM public key
+    // Set encryption public key
     command =
         'update:public:publickey${atSign} ${at_demo_data.encryptionPublicKeyMap[atSign]}\n';
     response = await _atLookup.executeCommand(command, auth: true);
     if (response!.startsWith('data:') && response != 'data:null') {
-      print('Setting of PKAM public key for $atSign is successful');
+      print('Setting of encryption public key for $atSign is successful');
     } else {
-      print('Failed to update PKAM public key for $atSign');
+      print('Failed to update encryption public key for $atSign');
       return;
     }
 
