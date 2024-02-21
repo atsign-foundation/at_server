31d841e3ae2fb47d29b942a73bd02300ac15f61b
Murali <muralidharan@shrisinfotech.com>
2024-02-21 21:31:13 +0530

    fix: revert some code in metadata builder for unset scenrio

--- packages/at_persistence_secondary_server/lib/src/model/at_metadata_builder.dart
+++ packages/at_persistence_secondary_server/lib/src/model/at_metadata_builder.dart
@@ -3,94 +3,139 @@ import 'package:at_utils/at_logger.dart';
 
 /// Builder class to build [AtMetaData] object.
 class AtMetadataBuilder {
-  late final AtMetaData atMetaData;
+  late AtMetaData atMetaData;
 
   /// We will constrain to millisecond precision because Hive only stores
   /// [DateTime]s to millisecond precision - see https://github.com/hivedb/hive/issues/474
   /// for details.
-  final DateTime currentUtcTimeToMillisecondPrecision =
+  var currentUtcTimeToMillisecondPrecision =
       DateTime.now().toUtcMillisecondsPrecision();
 
   static final AtSignLogger logger = AtSignLogger('AtMetadataBuilder');
 
   AtMetadataBuilder(
-      {String? atSign, AtMetaData? newMetaData, AtMetaData? existingMetaData})
-      : atMetaData = newMetaData ?? AtMetaData() {
+      {String? atSign, AtMetaData? newMetaData, AtMetaData? existingMetaData}) {
+    newMetaData ??= AtMetaData();
+    atMetaData = newMetaData;
     // createdAt indicates the date and time of the key created.
     // For a new key, the currentDateTime is set and remains unchanged
     // on an update event.
-    atMetaData.createdAt =
-        existingMetaData?.createdAt ?? currentUtcTimeToMillisecondPrecision;
+    (existingMetaData?.createdAt == null)
+        ? atMetaData.createdAt = currentUtcTimeToMillisecondPrecision
+        : atMetaData.createdAt = existingMetaData?.createdAt;
     atMetaData.createdBy ??= atSign;
     atMetaData.updatedBy = atSign;
-
     // updatedAt indicates the date and time of the key updated.
     // For a new key, the updatedAt is same as createdAt and on key
     // update, set the updatedAt to the currentDateTime.
     atMetaData.updatedAt = currentUtcTimeToMillisecondPrecision;
     atMetaData.status = 'active';
+    // // The version indicates the number of updates a key has received.
+    // // Version is set to 0 for a new key and for each update the key receives,
+    // // the version increases by 1
+    // (existingMetaData?.version == null)
+    //     ? atMetaData.version = 0
+    //     : atMetaData.version = (existingMetaData!.version! + 1);
 
-    // sets newAtMetaData attributes if set. Otherwise fallback to existingMetaData attributes.
-    _copyMetadata(existingMetaData, newMetaData);
+    //If new metadata is available, consider new metadata, else if existing metadata is available consider it.
+    int? ttl;
+    ttl ??= newMetaData.ttl;
+    if (ttl == null && existingMetaData != null) ttl = existingMetaData.ttl;
 
-    if (atMetaData.ttl != null && atMetaData.ttl! >= 0) {
-      setTTL(atMetaData.ttl, ttb: atMetaData.ttb);
+    int? ttb;
+    ttb ??= newMetaData.ttb;
+    if (ttb == null && existingMetaData != null) ttb = existingMetaData.ttb;
+
+    int? ttr;
+    ttr ??= newMetaData.ttr;
+    if (ttr == null && existingMetaData != null) ttr = existingMetaData.ttr;
+
+    bool? ccd;
+    ccd ??= newMetaData.isCascade;
+    if (ccd == null && existingMetaData != null) {
+      ccd = existingMetaData.isCascade;
     }
-    if (atMetaData.ttb != null && atMetaData.ttb! >= 0) {
-      setTTB(atMetaData.ttb);
+    bool? isBinary;
+    isBinary ??= newMetaData.isBinary;
+    if (isBinary == null && existingMetaData != null) {
+      isBinary = existingMetaData.isBinary;
+    }
+    bool? isEncrypted;
+    isEncrypted ??= newMetaData.isEncrypted;
+    if (isEncrypted == null && existingMetaData != null) {
+      isEncrypted = existingMetaData.isEncrypted;
+    }
+    String? dataSignature;
+    dataSignature ??= newMetaData.dataSignature;
+    if (dataSignature == null && existingMetaData != null) {
+      dataSignature = existingMetaData.dataSignature;
+    }
+    String? sharedKeyEncrypted;
+    sharedKeyEncrypted ??= newMetaData.sharedKeyEnc;
+    if (sharedKeyEncrypted == null && existingMetaData != null) {
+      sharedKeyEncrypted = existingMetaData.sharedKeyEnc;
+    }
+    String? publicKeyChecksum;
+    publicKeyChecksum ??= newMetaData.pubKeyCS;
+    if (publicKeyChecksum == null && existingMetaData != null) {
+      publicKeyChecksum = existingMetaData.pubKeyCS;
+    }
+    String? encoding;
+    encoding ??= newMetaData.encoding;
+    if (encoding == null && existingMetaData != null) {
+      encoding = existingMetaData.encoding;
+    }
+    String? encKeyName;
+    encKeyName ??= newMetaData.encKeyName;
+    if (encKeyName == null && existingMetaData != null) {
+      encKeyName = existingMetaData.encKeyName;
+    }
+    String? encAlgo;
+    encAlgo ??= newMetaData.encAlgo;
+    if (encAlgo == null && existingMetaData != null) {
+      encAlgo = existingMetaData.encAlgo;
+    }
+    String? ivNonce;
+    ivNonce ??= newMetaData.ivNonce;
+    if (ivNonce == null && existingMetaData != null) {
+      ivNonce = existingMetaData.ivNonce;
+    }
+    String? skeEncKeyName;
+    skeEncKeyName ??= newMetaData.skeEncKeyName;
+
+    if (skeEncKeyName == null && existingMetaData != null) {
+      skeEncKeyName = existingMetaData.skeEncKeyName;
+    }
+    String? skeEncAlgo;
+    skeEncAlgo ??= newMetaData.skeEncAlgo;
+    if (skeEncAlgo == null && existingMetaData != null) {
+      skeEncAlgo = existingMetaData.skeEncAlgo;
+    }
+    if (ttl != null && ttl >= 0) {
+      setTTL(ttl, ttb: ttb);
+    }
+    if (ttb != null && ttb >= 0) {
+      setTTB(ttb);
     }
     // If TTR is -1, cache the key forever.
-    if (atMetaData.ttr != null && atMetaData.ttr! > 0 || atMetaData.ttr == -1) {
-      setTTR(atMetaData.ttr);
+    if (ttr != null && ttr > 0 || ttr == -1) {
+      setTTR(ttr);
     }
+    if (ccd != null) {
+      setCCD(ccd);
+    }
+    atMetaData.isBinary = isBinary;
+    atMetaData.isEncrypted = isEncrypted;
+    atMetaData.dataSignature = dataSignature;
+    atMetaData.sharedKeyEnc = sharedKeyEncrypted;
+    atMetaData.pubKeyCS = publicKeyChecksum;
+    atMetaData.encoding = encoding;
+    atMetaData.encKeyName = encKeyName;
+    atMetaData.encAlgo = encAlgo;
+    atMetaData.ivNonce = ivNonce;
+    atMetaData.skeEncKeyName = skeEncKeyName;
+    atMetaData.skeEncAlgo = skeEncAlgo;
   }
-
-  // if existing metadata field is NOT null and new metadata field is null object, then set existing metadata field
-  // if existing metadata field is NOT null and new metadata field is not null object, then set new metadata field
-  // if existing metadata field is null and new metadata field is NOT null, then set new metadata field
-  // if existing metadata field is NOT null and new metadata field is null string, then set new metadata field (unset scenario)
-  void _copyMetadata(AtMetaData? existingMetaData, AtMetaData? newAtMetaData) {
-    atMetaData.ttl = _getOrDefault(newAtMetaData?.ttl, existingMetaData?.ttl);
-    atMetaData.ttb = newAtMetaData?.ttb ?? existingMetaData?.ttb;
-    atMetaData.ttr = newAtMetaData?.ttr ?? existingMetaData?.ttr;
-    atMetaData.isCascade =
-        newAtMetaData?.isCascade ?? existingMetaData?.isCascade;
-    atMetaData.isBinary = newAtMetaData?.isBinary ?? existingMetaData?.isBinary;
-    atMetaData.isEncrypted =
-        newAtMetaData?.isEncrypted ?? existingMetaData?.isEncrypted;
-    atMetaData.dataSignature = newAtMetaData?.dataSignature == "null"
-        ? null
-        : newAtMetaData?.dataSignature ?? existingMetaData?.dataSignature;
-    atMetaData.sharedKeyEnc = newAtMetaData?.sharedKeyEnc == "null"
-        ? null
-        : newAtMetaData?.sharedKeyEnc ?? existingMetaData?.sharedKeyEnc;
-    atMetaData.pubKeyCS = newAtMetaData?.pubKeyCS == "null"
-        ? null
-        : newAtMetaData?.pubKeyCS ?? existingMetaData?.pubKeyCS;
-
-    atMetaData.encoding = newAtMetaData?.encoding == "null"
-        ? null
-        : newAtMetaData?.encoding ?? existingMetaData?.encoding;
-    atMetaData.encKeyName = newAtMetaData?.encKeyName == "null"
-        ? null
-        : newAtMetaData?.encKeyName ?? existingMetaData?.encKeyName;
-    atMetaData.encAlgo = newAtMetaData?.encAlgo == "null"
-        ? null
-        : newAtMetaData?.encAlgo ?? existingMetaData?.encAlgo;
-    atMetaData.ivNonce = newAtMetaData?.ivNonce == "null"
-        ? null
-        : newAtMetaData?.ivNonce ?? existingMetaData?.ivNonce;
-    atMetaData.skeEncKeyName = newAtMetaData?.skeEncKeyName == "null"
-        ? null
-        : newAtMetaData?.skeEncKeyName ?? existingMetaData?.skeEncKeyName;
-    atMetaData.skeEncAlgo = newAtMetaData?.skeEncAlgo == "null"
-        ? null
-        : newAtMetaData?.skeEncAlgo ?? existingMetaData?.skeEncAlgo;
-    atMetaData.version = newAtMetaData?.version ?? existingMetaData?.version;
-  }
-
-  int? _getOrDefault(int? newValue, int? existingValue) =>
-      newValue ?? existingValue;
 
   void setTTL(int? ttl, {int? ttb}) {
     if (ttl != null) {
@@ -123,17 +168,32 @@ class AtMetadataBuilder {
     atMetaData.isCascade = ccd;
   }
 
-  DateTime? _getAvailableAt(int epochNow, int ttb) =>
-      DateTime.fromMillisecondsSinceEpoch(epochNow + ttb).toUtc();
+  DateTime? _getAvailableAt(int epochNow, int ttb) {
+    var availableAt = epochNow + ttb;
+    return DateTime.fromMillisecondsSinceEpoch(availableAt).toUtc();
+  }
 
   DateTime? _getExpiresAt(int epochNow, int ttl, {int? ttb}) {
-    if (ttl == 0) return null; // Key will not expire if TTL is 0
-    var expiresAt = epochNow + ttl + (ttb ?? 0);
+    //if ttl is zero, reset expires at. The key will not expire
+    if (ttl == 0) {
+      return null;
+    }
+    var expiresAt = epochNow + ttl;
+    if (ttb != null) {
+      expiresAt = expiresAt + ttb;
+    }
     return DateTime.fromMillisecondsSinceEpoch(expiresAt).toUtc();
   }
 
-  DateTime? _getRefreshAt(DateTime today, int ttr) =>
-      ttr == -1 ? null : today.add(Duration(seconds: ttr));
+  DateTime? _getRefreshAt(DateTime today, int ttr) {
+    if (ttr == -1) {
+      return null;
+    }
 
-  AtMetaData build() => atMetaData;
+    return today.add(Duration(seconds: ttr));
+  }
+
+  AtMetaData build() {
+    return atMetaData;
+  }
 }
