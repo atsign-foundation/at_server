3a85f734503b891324da35e32ac74ddfc274177b
Colin Constable <colin@constable.cc>
2025-04-04 22:40:35 -0700

    fix: Prevent atsign clash in the future with "@base" as that was used as a directory..

--- tools/build_ephemeral_environment/ve_base/contents/tmp/setup/createConf.sh
+++ tools/build_ephemeral_environment/ve_base/contents/tmp/setup/createConf.sh
@@ -9,11 +9,11 @@ fi
 #Use incoming Enviroment variables if set
 if [ -n "${DNS_FQDN+1}" ]; then
   echo "\$DNS_FQDN is set to $DNS_FQDN"
-  sed -i 's/DNS_FQDN/'$DNS_FQDN'/g' /atsign/secondary/@base/config/config.yaml
+  sed -i 's/DNS_FQDN/'$DNS_FQDN'/g' /atsign/secondary/base/config/config.yaml
 else
   echo "\$DNS_FQDN is not set, setting to default"
   DNS_FQDN="vip.ve.atsign.zone"
-  sed -i 's/DNS_FQN/'$DNS_FQDN'/g' /atsign/secondary/@base/config/config.yaml
+  sed -i 's/DNS_FQN/'$DNS_FQDN'/g' /atsign/secondary/base/config/config.yaml
   echo "\$DNS_FQDN is set to $DNS_FQDN"
 fi
 
@@ -35,14 +35,14 @@ printf "$ATSIGN		$CRAM\n"
 printf "$ATSIGN		$CRAM\n" >> /tmp/CRAM_Keys
 
 cp  TEMPLATE.conf /etc/supervisor/conf.d/${FIRST_PORT}_${ATSIGN}.conf
-mkdir /atsign/secondary/${ATSIGN}
-ln -s /atsign/secondary/@base/certs /atsign/secondary/${ATSIGN}/certs
-ln -s /atsign/secondary/@base/config /atsign/secondary/${ATSIGN}/config
+mkdir /atsign/atservers/${ATSIGN}
+ln -s /atsign/secondary/base/certs /atsign/atservers/${ATSIGN}/certs
+ln -s /atsign/secondary/base/config /atsign/atservers/${ATSIGN}/config
 
 sed -i 's/ATSIGN/'$ATSIGN'/g' /etc/supervisor/conf.d/${FIRST_PORT}_${ATSIGN}.conf
 sed -i 's/CRAM/'$CRAM'/g' /etc/supervisor/conf.d/${FIRST_PORT}_${ATSIGN}.conf
 sed -i 's/PORT/'$FIRST_PORT'/g' /etc/supervisor/conf.d/${FIRST_PORT}_${ATSIGN}.conf
-echo $CRAM > /atsign/secondary/${ATSIGN}/CRAM
+echo $CRAM > /atsign/atservers/${ATSIGN}/CRAM
 
 #
 #Add records to redis
