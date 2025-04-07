ea3941ae419f3839abc9c47fc461dbf5715f2648
Colin Constable <colin@constable.cc>
2025-04-07 14:42:21 -0700

    feat: removed unrequired files

--- tools/build_ephemeral_environment/ve_base/contents/tmp/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /tmp/setup/
-./createConf.sh ./atsigns
-
-# Run supervisord as root
-sed -i '/\[supervisord\]/a user=root' /etc/supervisor/supervisord.conf
-
-supervisord  -c /etc/supervisor/supervisord.conf -n 
