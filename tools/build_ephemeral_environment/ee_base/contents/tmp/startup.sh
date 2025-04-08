298f1eecb56d562bcf5c82fce327d316ce21f98b
Colin Constable <colin@constable.cc>
2025-04-07 18:27:00 -0700

    fix: some how removed the prevention of blocking of @base moved secondaries to atservers directory when running

--- tools/build_ephemeral_environment/ee_base/contents/tmp/startup.sh
+++ tools/build_ephemeral_environment/ee_base/contents/tmp/startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# Create atSign/atServers
 cd /tmp/setup/
 ./createConf.sh ./atsigns
+
+# Copy rootca/cacert.pem into place
+# This is needed if using -v to mount over the certs dir
+cp /atsign/secondary/base/rootca/cacert.pem /atsign/secondary/base/certs/
 
 # Run supervisord as root
 sed -i '/\[supervisord\]/a user=root' /etc/supervisor/supervisord.conf
