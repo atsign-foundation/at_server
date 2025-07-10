d639e3f3d0f778e28d5463e9973bd910c7ba3f75
gkc <gary.casey@gmail.com>
2025-07-10 13:58:50 +0100

    feat: build_virtual_env: added utility program generate_create_demo_accounts.sh.dart feat: build_virtual_env: tweaked install_PKAM_Keys.dart chore: update lints for virtual env & ephemeral env

--- tools/build_virtual_environment/install_PKAM_Keys/bin/install_PKAM_Keys.dart
+++ tools/build_virtual_environment/install_PKAM_Keys/bin/install_PKAM_Keys.dart
@@ -1,78 +1,145 @@
+import 'dart:io';
+
 import 'package:at_demo_data/at_demo_data.dart' as at_demo_data;
 import 'package:at_lookup/at_lookup.dart';
+import 'package:at_utils/at_utils.dart';
 
-void main(List<String> arguments) async {
-  await Future.forEach(at_demo_data.allAtsigns, (String atSign) async {
-    if (atSign != 'anonymous') {
-      await cramAuthAndUpdateKeys(atSign);
-    }
-  });
-  await Future.forEach(at_demo_data.apkamAtsigns, (String atSign) async {
-    await cramAuth(atSign);
-  });
+final String rootDomain = 'vip.ve.atsign.zone';
+final int rootPort = 64;
+final SecondaryAddressFinder saf =
+    CacheableSecondaryAddressFinder(rootDomain, rootPort);
+late final AtSignLogger logger;
+
+// we'll try to look up the atSign in the atDirectory every 5 seconds,
+// up to 24 times i.e. for up to 2 minutes
+final int retryDelay = 5;
+final int tries = 24;
+
+void main(List<String> args) async {
+  AtSignLogger.defaultLoggingHandler = AtSignLogger.stdErrLoggingHandler;
+  if (args.contains('--debug')) {
+    AtSignLogger.root_level = 'finest';
+  } else if (args.contains('-v') || args.contains('--verbose')) {
+    AtSignLogger.root_level = 'info';
+  } else {
+    AtSignLogger.root_level = 'warning';
+  }
+  logger = AtSignLogger(' install_PKAM_Keys ');
+
+  logger.shout('Starting. Will wait for up to 2 minutes for atSigns to be'
+      ' registered in the VE atDirectory');
+  List<Future<void>> futures = [];
+
+  futures.addAll(
+      at_demo_data.allAtsigns.map((String atSign) => cramAndPkamAuth(atSign)));
+
+  futures.addAll(
+      at_demo_data.apkamAtsigns.map((String atSign) => cramAuthOnly(atSign)));
+
+  await Future.wait(futures);
+
+  exit(0);
 }
 
-Future<void> cramAuth(String atSign) async {
+Future<bool> atSignIsInAtDirectory(String atSign) async {
+  for (int i = 0; i < tries; i++) {
+    try {
+      logger.info('Looking up $atSign in atDirectory $rootDomain:$rootPort}');
+      await saf.findSecondary(atSign);
+      logger.shout('Found $atSign in atDirectory $rootDomain:$rootPort}');
+      return true;
+    } catch (_) {
+      await Future.delayed(Duration(seconds: retryDelay));
+    }
+  }
+  logger.warning('$atSign not in atDirectory after ${tries * retryDelay} secs');
+  return false;
+}
+
+Future<void> cramAuthOnly(String atSign) async {
+  if (!await atSignIsInAtDirectory(atSign)) {
+    return;
+  }
   try {
-    var _atLookup = AtLookupImpl(atSign, 'vip.ve.atsign.zone', 64);
+    final atLookUp = AtLookupImpl(
+      atSign,
+      rootDomain,
+      rootPort,
+      secondaryAddressFinder: saf,
+    );
     var isCramAuthSuccessful =
-        await _atLookup.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
+        await atLookUp.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
     if (!isCramAuthSuccessful) {
-      print('CRAM Authentication failed for $atSign');
+      logger.severe('CRAM Authentication failed for $atSign');
       return;
     }
-    print('CRAM Authentication is successful for $atSign');
-    await _atLookup.close();
+    logger.shout('cramAuthOnly successful for $atSign');
+    await atLookUp.close();
   } on Exception catch (e) {
-    print('error while cram auth for ${atSign} exception: ${e.toString()}');
+    logger
+        .severe('error while cram auth for $atSign exception: ${e.toString()}');
   }
 }
 
-Future<void> cramAuthAndUpdateKeys(String atSign) async {
+Future<void> cramAndPkamAuth(String atSign) async {
+  if (atSign == 'anonymous') {
+    return;
+  }
+  if (!await atSignIsInAtDirectory(atSign)) {
+    return;
+  }
   try {
-    var _atLookup = AtLookupImpl(atSign, 'vip.ve.atsign.zone', 64);
+    final atLookUp = AtLookupImpl(
+      atSign,
+      'vip.ve.atsign.zone',
+      rootPort,
+      secondaryAddressFinder: saf,
+    );
     var isCramAuthSuccessful =
-        await _atLookup.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
+        await atLookUp.cramAuthenticate(at_demo_data.cramKeyMap[atSign]!);
     if (!isCramAuthSuccessful) {
-      print('CRAM Authentication failed for $atSign');
+      logger.severe('CRAM Authentication failed for $atSign');
       return;
     }
-    print('CRAM Authentication is successful for $atSign');
+    logger.info('CRAM Authentication is successful for $atSign');
 
     // Set PKAM public key
     var command =
         'update:privatekey:at_pkam_publickey ${at_demo_data.pkamPublicKeyMap[atSign]}\n';
-    var response = await _atLookup.executeCommand(command, auth: true);
+    var response = await atLookUp.executeCommand(command, auth: true);
     if (response == 'data:-1') {
-      print('Setting of PKAM public key for $atSign is successful');
+      logger.info('Setting of PKAM public key for $atSign is successful');
     } else {
-      print('Failed to update PKAM public key for $atSign');
+      logger.severe('Failed to update PKAM public key for $atSign');
       return;
     }
 
     // Set encryption public key
     command =
-        'update:public:publickey${atSign} ${at_demo_data.encryptionPublicKeyMap[atSign]}\n';
-    response = await _atLookup.executeCommand(command, auth: true);
+        'update:public:publickey$atSign ${at_demo_data.encryptionPublicKeyMap[atSign]}\n';
+    response = await atLookUp.executeCommand(command, auth: true);
     if (response!.startsWith('data:') && response != 'data:null') {
-      print('Setting of encryption public key for $atSign is successful');
+      logger.info('Setting of encryption public key for $atSign is successful');
     } else {
-      print('Failed to update encryption public key for $atSign');
+      logger.severe('Failed to update encryption public key for $atSign');
       return;
     }
 
     // Set pkamInstalled key to "yes"
     command = 'update:public:pkaminstalled$atSign yes\n';
-    response = await _atLookup.executeCommand(command, auth: true);
+    response = await atLookUp.executeCommand(command, auth: true);
     if (response!.startsWith('data:') && response != 'data:null') {
-      print('Set pkaminstalled key for $atSign is successful');
+      logger.info('Set pkaminstalled key for $atSign is successful');
     } else {
-      print('Failed to update pkaminstalled key for $atSign');
+      logger.info('Failed to update pkaminstalled key for $atSign');
       return;
     }
 
-    await _atLookup.close();
+    logger.shout('cramAndPkamAuth successful for $atSign');
+
+    await atLookUp.close();
   } on Exception catch (e) {
-    print('error while setting keys for ${atSign} exception: ${e.toString()}');
+    logger.info(
+        'error while setting keys for $atSign exception: ${e.toString()}');
   }
 }
