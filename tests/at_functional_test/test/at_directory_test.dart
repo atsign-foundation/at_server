83bd53e2285d5888fd5a8ee37eb7ba22ed7965da
gkc <gary.casey@gmail.com>
2025-08-15 12:06:24 +0100

    test: at_directory_test.dart: cleanup, restructure, bugfix

--- tests/at_functional_test/test/at_directory_test.dart
+++ tests/at_functional_test/test/at_directory_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 
 import 'package:at_commons/at_commons.dart';
 import 'package:at_lookup/at_lookup.dart';
@@ -17,106 +18,172 @@ void main() {
   AtSignLogger.root_level = 'shout';
   final AtSignLogger logger = AtSignLogger(' at_directory_test ');
   logger.level = 'info';
-  List<String> atSigns = at_demo_data.allAtsigns
-    ..addAll(at_demo_data.apkamAtsigns)
-    ..remove('anonymous');
   final root = 'vip.ve.atsign.zone';
 
-  // - Annoyingly, the Dart http clients do not set alpn protocols
-  // - And the client in the `http` package does not allow setting a context;
-  // it uses the default context. We can setAlpnProtocols in the default
-  // context, but this means every connection to the atServer uses those
-  // protocols, so normal AtLookup connections fail
-  // - So, we use dart.io's HttpClient which allows us to pass a context
-  // - In order to demonstrate that AtLookup is not interfered with, we
-  //   do this initialization of the HttpClient before any of the tests run.
+  group('basic atDirectory tests', () {
+    List<String> atSigns = at_demo_data.allAtsigns
+      ..addAll(at_demo_data.apkamAtsigns)
+      ..remove('anonymous');
+
+    test('lookup existing atSign via 64', () async {
+      List<Future> futures = [];
+      for (final atSign in atSigns) {
+        final saf = CacheableSecondaryAddressFinder(root, 64);
+        futures.add(saf.findSecondary(atSign));
+      }
+      final responses = await Future.wait(futures);
+      logger.info('${responses.length} of ${atSigns.length} OK');
+    });
+
+    test('lookup non-existent atSign avia 64', () async {
+      List<Future> futures = [];
+      for (final atSign in atSigns.map((e) => '${e}_nope')) {
+        final saf = CacheableSecondaryAddressFinder(root, 64);
+        futures.add(expectLater(saf.findSecondary(atSign),
+            throwsA(isA<SecondaryNotFoundException>())));
+      }
+      final responses = await Future.wait(futures);
+      logger.info('${responses.length} of ${atSigns.length} OK');
+    });
+
+    test('lookup existing atSign via https', () async {
+      List<Future> futures = [];
+      for (final atSign in atSigns) {
+        final Uri url = Uri.https(root, atSign);
+        futures.add(expectLater(
+          http.get(url).then((response) => response.statusCode),
+          completion(HttpStatus.ok),
+        ));
+      }
+      final responses = await Future.wait(futures);
+      logger.info('${responses.length} of ${atSigns.length} OK');
+    });
+
+    test('lookup non-existent atSign via https', () async {
+      List<Future> futures = [];
+      for (final atSign in atSigns.map((e) => '${e}_nope')) {
+        final Uri url = Uri.https(root, atSign);
+        futures.add(expectLater(
+          http.get(url).then((response) => response.statusCode),
+          completion(HttpStatus.notFound),
+        ));
+      }
+      final responses = await Future.wait(futures);
+      logger.info('${responses.length} of ${atSigns.length} OK');
+    });
+  });
+
+  group('atDirectory redirect tests', () {
+    List<String> atSigns = at_demo_data.allAtsigns
+      ..addAll(at_demo_data.apkamAtsigns)
+      ..remove('anonymous');
+
+    Future<bool> doTheGet(String atSign) async {
+      final String command = 'lookup:signing_publickey$atSign';
+      final atLookup = AtLookupImpl(atSign, root, 64);
+      final String pskFromAtLookup;
+      try {
+        pskFromAtLookup = (await atLookup.executeCommand('$command\n'))!;
+      } finally {
+        await atLookup.close();
+      }
+
+      final (statusCode, pskFromHttpRedirect) = await dartIoHttpClientGet(
+          Uri.https(root, '/$atSign/signing_publickey'));
+
+      final bool statusOk = statusCode == HttpStatus.ok;
+      final bool pskMatched = pskFromHttpRedirect == pskFromAtLookup;
+
+      if (!statusOk || !pskMatched) {
+        logger.warning('Check failed for $atSign : status: $statusCode'
+            ' pskFromHttpRedirect: $pskFromHttpRedirect'
+            ' pskFromAtLookup: $pskFromAtLookup');
+      }
+
+      return statusOk && pskMatched;
+    }
+
+    // For each atSign
+    // - Fetch public:signing_publickey from each atServer via AtLookup
+    // - Fetch it from https://<rootHost>/$atSign/signing_publickey
+    //   - The presence of the additional 'signing_publickey' in the uri path
+    //     tells the atDirectory that it should
+    //     - 1) look up the atSign's atServer's host and port, and if successful
+    //     - 2) Return a 302 with the location header set to https:<atServerHostAndPort>/signing_publickey
+    //   - Default behaviour of the Dart http clients (whether HttpClient from dart.io
+    //     or Client from the http package) is to follow GET redirects
+    // - Assert that the values we fetched via AtLookup and http are identical
+    test('lookup signing_publickey via https with redirect in isolates',
+        () async {
+      int successes = 0;
+      int isolateCounter = 0;
+      for (final atSign in atSigns) {
+        final bool succeeded = await Isolate.run(() async {
+          AtSignLogger.defaultLoggingHandler =
+              AtSignLogger.stdErrLoggingHandler;
+          AtSignLogger.root_level = 'shout';
+          AtSignLogger logger = AtSignLogger(
+              Isolate.current.debugName ?? 'lookup psk for $atSign');
+          logger.level = 'info';
+
+          return await doTheGet(atSign);
+        }, debugName: 'isolate ${++isolateCounter}');
+        if (succeeded) {
+          successes++;
+        }
+      }
+
+      if (successes != atSigns.length) {
+        throw Exception('Only $successes successes out of ${atSigns.length}');
+      }
+      logger.info('$successes successes out of ${atSigns.length} OK');
+    });
+
+    test('lookup signing_publickey via https with redirect', () async {
+      int successes = 0;
+      for (final atSign in atSigns) {
+        final bool succeeded = await doTheGet(atSign);
+        if (succeeded) {
+          successes++;
+        }
+      }
+
+      if (successes != atSigns.length) {
+        throw Exception('Only $successes successes out of ${atSigns.length}');
+      }
+      logger.info('$successes successes out of ${atSigns.length} OK');
+    });
+  });
+}
+
+/// - Annoyingly, the Dart http clients do not set alpn protocols
+/// - And the client in the `http` package does not allow setting a context;
+/// it uses the default context. We can setAlpnProtocols in the default
+/// context, but this means every connection to the atServer uses those
+/// protocols, so normal AtLookup connections fail
+/// - So, we use dart.io's HttpClient which allows us to pass a context
+/// - returns (statusCode, body)
+Future<(int, String)> dartIoHttpClientGet(Uri uri, {HttpClient? client}) async {
+  bool shouldClose = client == null;
+  client ??= newHttpClient();
+  try {
+    client.idleTimeout = Duration(seconds: 0);
+
+    HttpClientRequest request = await client.getUrl(uri);
+    request.headers.persistentConnection = false;
+    HttpClientResponse response = await request.close();
+    final (statusCode, body) =
+        (response.statusCode, await response.transform(utf8.decoder).join());
+    return (statusCode, body);
+  } finally {
+    if (shouldClose) {
+      client.close(force: true);
+    }
+  }
+}
+
+HttpClient newHttpClient() {
   final SecurityContext context = SecurityContext(withTrustedRoots: true);
   context.setAlpnProtocols(['http/1.1'], false);
-  final HttpClient client = HttpClient(context: context);
-
-  Future<(int, String)> httpClientGet(Uri uri) async {
-    HttpClientRequest request = await client.getUrl(uri);
-    HttpClientResponse response = await request.close();
-    return (response.statusCode, await response.transform(utf8.decoder).join());
-  }
-
-  test('lookup existing atSign via 64', () async {
-    List<Future> futures = [];
-    for (final atSign in atSigns) {
-      final saf = CacheableSecondaryAddressFinder(root, 64);
-      futures.add(saf.findSecondary(atSign));
-    }
-    final responses = await Future.wait(futures);
-    logger.info('${responses.length} of ${atSigns.length} OK');
-  });
-
-  test('lookup non-existent atSign avia 64', () async {
-    List<Future> futures = [];
-    for (final atSign in atSigns.map((e) => '${e}_nope')) {
-      final saf = CacheableSecondaryAddressFinder(root, 64);
-      futures.add(expectLater(saf.findSecondary(atSign),
-          throwsA(isA<SecondaryNotFoundException>())));
-    }
-    final responses = await Future.wait(futures);
-    logger.info('${responses.length} of ${atSigns.length} OK');
-  });
-
-  test('lookup existing atSign via https', () async {
-    List<Future> futures = [];
-    for (final atSign in atSigns) {
-      final Uri url = Uri.https(root, atSign);
-      futures.add(expectLater(
-        http.get(url).then((response) => response.statusCode),
-        completion(HttpStatus.ok),
-      ));
-    }
-    final responses = await Future.wait(futures);
-    logger.info('${responses.length} of ${atSigns.length} OK');
-  });
-
-  test('lookup non-existent atSign via https', () async {
-    List<Future> futures = [];
-    for (final atSign in atSigns.map((e) => '${e}_nope')) {
-      final Uri url = Uri.https(root, atSign);
-      futures.add(expectLater(
-        http.get(url).then((response) => response.statusCode),
-        completion(HttpStatus.notFound),
-      ));
-    }
-    final responses = await Future.wait(futures);
-    logger.info('${responses.length} of ${atSigns.length} OK');
-  });
-
-  // For each atSign
-  // - Fetch public:signing_publickey from each atServer via AtLookup
-  // - Fetch it from https://<rootHost>/$atSign/signing_publickey
-  //   - The presence of the additional 'signing_publickey' in the uri path
-  //     tells the atDirectory that it should
-  //     - 1) look up the atSign's atServer's host and port, and if successful
-  //     - 2) Return a 302 with the location header set to https:<atServerHostAndPort>/signing_publickey
-  //   - Default behaviour of the Dart http clients (whether HttpClient from dart.io
-  //     or Client from the http package) is to follow GET redirects
-  // - Assert that the values we fetched via AtLookup and http are identical
-  test('lookup signing_publickey via https with redirect', () async {
-    Future<void> doit(String atSign) async {
-      final pskFromAtLookup = await AtLookupImpl(atSign, root, 64)
-          .executeCommand('lookup:signing_publickey$atSign\n');
-
-      final (statusCode, body) =
-          await httpClientGet(Uri.https(root, '/$atSign/signing_publickey'));
-
-      expect(statusCode, HttpStatus.ok);
-      final pskFromHttpRedirect = body;
-      expect(pskFromHttpRedirect, startsWith('data:'));
-      expect(pskFromHttpRedirect, pskFromAtLookup);
-    }
-
-    List<Future> futures = [];
-    for (final atSign in atSigns) {
-      futures.add(doit(atSign));
-    }
-
-    final responses = await Future.wait(futures);
-    logger.info('${responses.length} of ${atSigns.length} OK');
-  });
+  return HttpClient(context: context);
 }
