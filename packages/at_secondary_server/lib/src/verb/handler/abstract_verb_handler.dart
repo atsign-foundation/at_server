0803d38cb8a2ef49c424a06651ff60b0e8b6a931
gkc <gary.casey@gmail.com>
2026-04-28 14:34:27 +0100

    fix: Since the recent elimination of "testingMode", the ConfigVerbHandler relies on the standard auth checker in AbstractVerbHandler. However the Config verb was never added to the lists in _isReadAllowed and _isWriteAllowed.

--- packages/at_secondary_server/lib/src/verb/handler/abstract_verb_handler.dart
+++ packages/at_secondary_server/lib/src/verb/handler/abstract_verb_handler.dart
@@ -372,6 +372,7 @@ abstract class AbstractVerbHandler implements VerbHandler {
   bool _isReadAllowed(Verb verb, String access) {
     return (verb is LocalLookup ||
             verb is Lookup ||
+            verb is Config ||
             verb is NotifyFetch ||
             verb is NotifyStatus ||
             verb is NotifyList ||
@@ -384,6 +385,7 @@ abstract class AbstractVerbHandler implements VerbHandler {
   bool _isWriteAllowed(Verb verb, String access) {
     return (verb is Update ||
             verb is Delete ||
+            verb is Config ||
             verb is Notify ||
             verb is NotifyAll ||
             verb is NotifyRemove ||
